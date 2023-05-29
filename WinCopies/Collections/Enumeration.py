39dac9e6dba69aa3853cd5a4f3a1f45f8072d0d2
pierresprim <wincopies@wincopies.com>
2023-05-29 18:51:29 +0200

    Update directory structure

--- WinCopies/Collections/Enumeration.py
+++ WinCopies/Collections/Enumeration.py
@@ -5,45 +5,63 @@ Created on Sun Feb  6 20:37:51 2022
 @author: Pierre Sprimont
 """
 
-from typing import final, protected
+from abc import ABC, abstractmethod
+from typing import final #, protected
 
-class IEnumerator:
+class IEnumerator(ABC):
+    @abstractmethod
     def GetCurrent(self):
         pass
+    @abstractmethod
     def MoveNext(self) -> bool:
         pass
+    @abstractmethod
     def Reset(self) -> bool:
         pass
+    @abstractmethod
     def IsResetSupported(self) -> bool:
         pass
     
 class Enumerator(IEnumerator):
     def __init__(self):
-        self._enumerationCompleted : bool = False
+        self._enumerationCompleted: bool = False
+    #@protected
+    @abstractmethod
+    def MoveNextOverride(self) -> bool:
+        pass
+    #@protected
+    @abstractmethod
+    def ResetOverride(self) -> bool:
+        pass
+    @abstractmethod
     def GetCurrent(self):
         pass
-    @protected
-    def MoveNextOverride(self) -> bool:
+    @abstractmethod
+    def IsResetSupported(self) -> bool:
         pass
     @final
     def MoveNext(self) -> bool:
         if self._enumerationCompleted:
             return False
+        
         if self.MoveNextOverride():
             return True
+        
         self._enumerationCompleted = True
         return False
-    def IsResetSupported(self) -> bool:
-        pass
-    @protected
-    def ResetOverride(self) -> bool:
-        pass
+    
+    @final
     def Reset(self) -> bool:
         if self.IsResetSupported():
             self.ResetOverride()
+            self._enumerationCompleted = False
             return True
+        
         return False
+    
+    @final
     def __next__(self):
         if self.MoveNext():
             return self.GetCurrent()
-        return StopIteration()
+        else:
+            raise StopIteration
