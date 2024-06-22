15e1068854d3cf172cd6c7711097d7d052952ac2
pierresprim <49271108+pierresprim@users.noreply.github.com>
2024-06-22 10:37:41 +0200

    Add new functions

--- WinCopies/String.py
+++ WinCopies/String.py
@@ -1,3 +1,8 @@
+from typing import Callable
+from collections.abc import Iterable
+
+from WinCopies.Collections import Loop
+
 def Replace(string: str, esc: str, newEsc: str, *args: str) -> str:
     string = string.replace(esc + esc, esc)
     
@@ -6,3 +11,82 @@ def Replace(string: str, esc: str, newEsc: str, *args: str) -> str:
         string = string.replace(esc + arg, newEsc + arg)
     
     return string
+
+def SurroundWith(prefix: str, string: str, suffix: str) -> str:
+    return f"{prefix}{string}{suffix}"
+def TrySurroundWith(prefix: str, string: str|None, suffix: str) -> str|None:
+    return None if string is None else SurroundWith(prefix, string, suffix)
+
+def Surround(string: str, value: str) -> str:
+    return SurroundWith(value, string, value)
+def TrySurround(string: str|None, value: str) -> str|None:
+    return TrySurroundWith(value, string, value)
+
+def QuoteSurround(string: str) -> str:
+    return Surround(string, "'")
+def TryQuoteSurround(string: str) -> str:
+    return TrySurround(string, "'")
+
+def DoubleQuoteSurround(string: str) -> str:
+    return Surround(string, '"')
+def TryDoubleQuoteSurround(string: str) -> str:
+    return TrySurround(string, '"')
+
+def SpaceJoin(values: Iterable[str]) -> str:
+    return ' '.join(values)
+def SpaceJoinValues(*values: str) -> str:
+    return SpaceJoin(values)
+
+def CommaJoin(values: Iterable[str]) -> str:
+    return ','.join(values)
+def CommaJoinValues(*values: str) -> str:
+    return CommaJoin(values)
+
+def SemicolonJoin(values: Iterable[str]) -> str:
+    return ';'.join(values)
+def SemicolonJoinValues(*values: str) -> str:
+    return SemicolonJoin(values)
+
+def ColonJoin(values: Iterable[str]) -> str:
+    return ':'.join(values)
+def ColonJoinValues(*values: str) -> str:
+    return ColonJoin(values)
+def Join(values: Iterable[str]) -> str:
+    result: str = ''
+
+    for value in values:
+        result += value
+    
+    return result
+def JoinValues(*values: str) -> str:
+    return Join(values)
+
+def BuildFrom(values: Iterable[str], selector: Callable[[str], str]) -> str:
+    result: str = ''
+
+    for value in values:
+        result += selector(value)
+    
+    return result
+def BuildFromValues(*values: str, selector: Callable[[str], str]) -> str:
+    return BuildFrom(values, selector)
+
+def ConcatenateFrom(values: Iterable[str], separator: str, selector: Callable[[str], str]) -> str:
+    result: str = ''
+    
+    def firstAction(value: str):
+        nonlocal result
+
+        result += value
+
+    def action(value: str):
+        nonlocal result
+        nonlocal separator
+
+        result += separator + value
+
+    Loop.DoForEachAndFirst(values, firstAction, action)
+    
+    return result
+def ConcatenateFromValues(*values: str, separator: str, selector: Callable[[str], str]) -> str:
+    return ConcatenateFrom(values, separator, selector)
