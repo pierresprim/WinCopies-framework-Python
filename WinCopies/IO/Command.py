3b9e72643da4e202d52a319da776cf3dae54d7f1
pierresprim <49271108+pierresprim@users.noreply.github.com>
2024-06-22 11:06:01 +0200

    Update Run function

--- WinCopies/IO/Command.py
+++ WinCopies/IO/Command.py
@@ -5,8 +5,8 @@ from collections.abc import Iterable
 
 from WinCopies import DualResult, KeyValuePair, String
 
-def Run(command: str, captureOutput = False, throwOnError = True) -> DualResult[object, int]:
-    result: subprocess.CompletedProcess = subprocess.run(command, capture_output=captureOutput, shell=True, text=captureOutput)
+def Run(command: str|Iterable[str], captureOutput = False, shell=False, throwOnError = True) -> DualResult[object, int]:
+    result: subprocess.CompletedProcess = subprocess.run(command, capture_output=captureOutput, shell=shell, text=captureOutput, stdout=None if captureOutput else sys.__stdout__)
     
     if throwOnError:
         result.check_returncode()
