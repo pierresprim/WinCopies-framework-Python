76ef545eab29d40d56e3f7cf2f7985a661c3de22
pierresprim <49271108+pierresprim@users.noreply.github.com>
2025-01-02 21:18:43 +0100

    Add generic aliases

--- WinCopies/Delegates.py
+++ WinCopies/Delegates.py
@@ -1,16 +1,18 @@
 from typing import Callable
 
-def Self(value):
+from WinCopies import Predicate
+
+def Self[T](value: T) -> T:
     return value
 
-def PredicateAction[T](obj: T, predicate: Callable[[T], bool], action: Callable[[T], None]) -> bool:
+def PredicateAction[T](obj: T, predicate: Predicate[T], action: Callable[[T], None]) -> bool:
     if predicate(obj):
         action(obj)
 
         return True
     
     return False
-def GetPredicateAction[T](predicate: Callable[[T], bool], action: Callable[[T], None]) -> Callable[[T], bool]:
+def GetPredicateAction[T](predicate: Predicate[T], action: Callable[[T], None]) -> Predicate[T]:
     return lambda obj: PredicateAction(obj, predicate, action)
 
 def BoolFuncAction(func: Callable[[], bool], action: Callable[[], None]) -> bool:
@@ -25,51 +27,51 @@ def GetBoolFuncAction(func: Callable[[], bool], action: Callable[[], None]) -> C
 
 
 
-def __GetPredicate[T](p1: Callable[[T], bool], p2: Callable[[T], bool], converter: Callable[[T, Callable[[T], bool], Callable[[T], bool]], bool]) -> Callable[[T], bool]:
+def __GetPredicate[T](p1: Predicate[T], p2: Predicate[T], converter: Callable[[T, Predicate[T], Predicate[T]], bool]) -> Predicate[T]:
     return lambda obj: converter(obj, p1, p2)
 
-def PredicateAndAlso[T](obj: T, p1: Callable[[T], bool], p2: Callable[[T], bool]) -> bool:
+def PredicateAndAlso[T](obj: T, p1: Predicate[T], p2: Predicate[T]) -> bool:
     return p1(obj) and p2(obj)
-def GetAndAlsoPredicate[T](p1: Callable[[T], bool], p2: Callable[[T], bool]) -> Callable[[T], bool]:
+def GetAndAlsoPredicate[T](p1: Predicate[T], p2: Predicate[T]) -> Predicate[T]:
     return __GetPredicate(p1, p2, PredicateAndAlso)
 
-def PredicateAnd[T](obj: T, p1: Callable[[T], bool], p2: Callable[[T], bool]) -> bool:
+def PredicateAnd[T](obj: T, p1: Predicate[T], p2: Predicate[T]) -> bool:
     return p1(obj) & p2(obj)
-def GetAndPredicate[T](p1: Callable[[T], bool], p2: Callable[[T], bool]) -> Callable[[T], bool]:
+def GetAndPredicate[T](p1: Predicate[T], p2: Predicate[T]) -> Predicate[T]:
     return __GetPredicate(p1, p2, PredicateAnd)
 
-def PredicateOrElse[T](obj: T, p1: Callable[[T], bool], p2: Callable[[T], bool]) -> bool:
+def PredicateOrElse[T](obj: T, p1: Predicate[T], p2: Predicate[T]) -> bool:
     return p1(obj) or p2(obj)
-def GetOrElsePredicate[T](p1: Callable[[T], bool], p2: Callable[[T], bool]) -> Callable[[T], bool]:
+def GetOrElsePredicate[T](p1: Predicate[T], p2: Predicate[T]) -> Predicate[T]:
     return __GetPredicate(p1, p2, PredicateOrElse)
 
-def PredicateOr[T](obj: T, p1: Callable[[T], bool], p2: Callable[[T], bool]) -> bool:
+def PredicateOr[T](obj: T, p1: Predicate[T], p2: Predicate[T]) -> bool:
     return p1(obj) | p2(obj)
-def GetOrPredicate[T](p1: Callable[[T], bool], p2: Callable[[T], bool]) -> Callable[[T], bool]:
+def GetOrPredicate[T](p1: Predicate[T], p2: Predicate[T]) -> Predicate[T]:
     return __GetPredicate(p1, p2, PredicateOr)
 
-def PredicateNotAndAlso[T](obj: T, p1: Callable[[T], bool], p2: Callable[[T], bool]) -> bool:
+def PredicateNotAndAlso[T](obj: T, p1: Predicate[T], p2: Predicate[T]) -> bool:
     return (not p1(obj)) and p2(obj)
-def GetNotAndAlsoPredicate[T](p1: Callable[[T], bool], p2: Callable[[T], bool]) -> Callable[[T], bool]:
+def GetNotAndAlsoPredicate[T](p1: Predicate[T], p2: Predicate[T]) -> Predicate[T]:
     return __GetPredicate(p1, p2, PredicateNotAndAlso)
 
-def PredicateNotAnd[T](obj: T, p1: Callable[[T], bool], p2: Callable[[T], bool]) -> bool:
+def PredicateNotAnd[T](obj: T, p1: Predicate[T], p2: Predicate[T]) -> bool:
     return (not p1(obj)) & p2(obj)
-def GetNotAndPredicate[T](p1: Callable[[T], bool], p2: Callable[[T], bool]) -> Callable[[T], bool]:
+def GetNotAndPredicate[T](p1: Predicate[T], p2: Predicate[T]) -> Predicate[T]:
     return __GetPredicate(p1, p2, PredicateNotAnd)
 
-def PredicateNot[T](obj: T, predicate: Callable[[T], bool]) -> bool:
+def PredicateNot[T](obj: T, predicate: Predicate[T]) -> bool:
     return not predicate(obj)
-def GetNotPredicate[T](predicate: Callable[[T], bool]) -> Callable[[T], bool]:
+def GetNotPredicate[T](predicate: Predicate[T]) -> Predicate[T]:
     return lambda obj: PredicateNot(obj, predicate)
 
 
 
-def GetIndexedValueIndexComparison[T](index: int) -> Callable[[T], bool]:
+def GetIndexedValueIndexComparison[T](index: int) -> Predicate[T]:
     return lambda i, value: i == index
 
-def GetIndexedValueValueComparison[T](value: T) -> Callable[[T], bool]:
+def GetIndexedValueValueComparison[T](value: T) -> Predicate[T]:
     return lambda i, _value: value == _value
 
-def GetIndexedValueComparison[T](index: int, value: T) -> Callable[[T], bool]:
+def GetIndexedValueComparison[T](index: int, value: T) -> Predicate[T]:
     return lambda i, _value: i == index and value == _value
