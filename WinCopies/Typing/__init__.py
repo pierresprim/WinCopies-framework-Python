5a8f0048d47dace45883ad0559304e9713e52fdc
pierresprim <49271108+pierresprim@users.noreply.github.com>
2025-01-09 16:32:42 +0100

    New functions

--- WinCopies/Typing/__init__.py
+++ WinCopies/Typing/__init__.py
@@ -1,3 +1,5 @@
+from typing import Iterable, Type
+
 from WinCopies.Delegates import Self
 
 class Singleton(type):
@@ -27,3 +29,31 @@ def constant(f):
     def fset(self, value):
         raise TypeError
     return property(fget, fset)
+
+def IsSubclass[T](cls: Type[T], types: Iterable[Type[T]]) -> bool:
+    for type in types:
+        if issubclass(cls, type):
+            return True
+    
+    return False
+def IsSubclassOf[T](cls: Type[T], *types: Type[T]) -> bool:
+    return IsSubclass(cls, types)
+
+def Is[T](obj: T, types: Iterable[Type[T]]) -> bool:
+    return IsSubclass(type(obj), types)
+def IsOf[T](obj: T, *types: Type[T]) -> bool:
+    return Is(obj, types)
+
+def Implements[T](cls: Type[T], types: Iterable[Type[T]]) -> bool:
+    for type in types:
+        if not issubclass(cls, type):
+            return False
+    
+    return True
+def ImplementsAll[T](cls: Type[T], *types: Type[T]) -> bool:
+    return Implements(cls, types)
+
+def IsFrom[T](obj: T, types: Iterable[Type[T]]) -> bool:
+    return Implements(type(obj), types)
+def IsFromAll[T](obj: T, *types: Type[T]) -> bool:
+    return IsFrom(obj, types)
