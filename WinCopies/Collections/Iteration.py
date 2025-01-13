ab4db9d566c05691086c3a3392aa9b17ef2878e9
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2025-01-13 20:49:07 +0100

    New functions

--- WinCopies/Collections/Iteration.py
+++ WinCopies/Collections/Iteration.py
@@ -1,6 +1,8 @@
 from collections.abc import Iterable
 
+from WinCopies import Delegates
 from WinCopies.Collections import Generator
+from WinCopies.Typing.Delegate import Converter, Predicate
 
 def Append[T](items: Iterable[T], values: Iterable[T]) -> Generator[T]:
     for item in items:
@@ -15,3 +17,14 @@ def Prepend[T](items: Iterable[T], values: Iterable[T]) -> Generator[T]:
     return Append(values, items)
 def PrependValues[T](items: Iterable[T], *values: T) -> Generator[T]:
     return Prepend(items, values)
+
+def Select[TIn, TOut](items: Iterable[TIn], converter: Converter[TIn, TOut]) -> Generator[TOut]:
+    for item in items:
+        yield converter(item)
+
+def Include[T](items: Iterable[T], predicate: Predicate[T]) -> Generator[T]:
+    for item in items:
+        if predicate(item):
+            yield item
+def Exclude[T](items: Iterable[T], predicate: Predicate[T]) -> Generator[T]:
+    return Include(items, Delegates.GetNotPredicate(predicate))
