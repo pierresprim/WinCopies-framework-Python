b7984e83370f4d53ec954f1b8c4a0ae6c6a0eac6
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2025-01-13 20:48:46 +0100

    Update type hints and import directive

--- WinCopies/Collections/Iteration.py
+++ WinCopies/Collections/Iteration.py
@@ -1,15 +1,17 @@
-from typing import Iterable
+from collections.abc import Iterable
 
-def Append[T](items: Iterable[T], values: Iterable[T]) -> Iterable[T]:
+from WinCopies.Collections import Generator
+
+def Append[T](items: Iterable[T], values: Iterable[T]) -> Generator[T]:
     for item in items:
         yield item
     
     for value in values:
         yield value
-def AppendValues[T](items: Iterable[T], *values: T) -> Iterable[T]:
+def AppendValues[T](items: Iterable[T], *values: T) -> Generator[T]:
     return Append(items, values)
 
-def Prepend[T](items: Iterable[T], values: Iterable[T]) -> Iterable[T]:
+def Prepend[T](items: Iterable[T], values: Iterable[T]) -> Generator[T]:
     return Append(values, items)
-def PrependValues[T](items: Iterable[T], *values: T) -> Iterable[T]:
+def PrependValues[T](items: Iterable[T], *values: T) -> Generator[T]:
     return Prepend(items, values)
