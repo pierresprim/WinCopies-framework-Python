8a8ce251df1a9a2dedfe7e929257da8ca0ff6fcc
pierresprim <49271108+pierresprim@users.noreply.github.com>
2025-01-17 22:58:45 +0100

    Update Command.Run functions

--- WinCopies/IO/Command.py
+++ WinCopies/IO/Command.py
@@ -3,21 +3,30 @@ import subprocess
 
 from collections.abc import Iterable
 
-from WinCopies import String
+from WinCopies import Typing
+from WinCopies.Collections import Iteration
 from WinCopies.Typing.Pairing import DualResult, KeyValuePair
 
 def Run(command: str|Iterable[str], captureOutput = False, shell = False, throwOnError = True) -> DualResult[object, int]:
-    result: subprocess.CompletedProcess = subprocess.run(command, capture_output = captureOutput, shell = shell, text = captureOutput, stdout = None if captureOutput else sys.__stdout__)
+    args: str|list[str]|None = None
     
-    if throwOnError:
-        result.check_returncode()
+    if Typing.IsOf(command, str, list):
+        args = command
+    
+    elif Typing.IsOf(command, Iterable):
+        args = list(command)
+
+    else:
+        raise ValueError(f"command must be a string, Iterable[str] or list[str]. Command is {type(command)}", command)
+    
+    result: subprocess.CompletedProcess = subprocess.run(args, capture_output = captureOutput, shell = shell, text = captureOutput, check = throwOnError, stdout = None if captureOutput else sys.__stdout__)
     
     return DualResult(result.stdout, result.returncode)
 
-def RunWithArgs(command: str, args: Iterable[str], captureOutput = False, throwOnError = True) -> DualResult[object, int]:
-    return Run(String.SurroundWith(command, ' ', String.SpaceJoin(args)), captureOutput, throwOnError)
-def RunWithArgValues(command: str, captureOutput, throwOnError, *args: str) -> DualResult[object, int]:
-    return RunWithArgs(command, args, captureOutput, throwOnError)
+def RunWithArgs(command: str, args: Iterable[str], captureOutput = False, shell = False, throwOnError = True) -> DualResult[object, int]:
+    return Run(Iteration.PrependValues(args, command), captureOutput, shell, throwOnError)
+def RunWithArgValues(command: str, captureOutput, shell, throwOnError, *args: str) -> DualResult[object, int]:
+    return RunWithArgs(command, args, captureOutput, shell, throwOnError)
 
 def GetArgument(name: str, argument: str|None) -> str:
     return f"--{name}{None if argument is None else ' ' + argument}"
