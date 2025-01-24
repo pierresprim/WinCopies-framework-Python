130f1a9f2eb75ab73c372f82ee0b298f3791cb5c
pierresprim <49271108+pierresprim@users.noreply.github.com>
2025-01-24 13:37:08 +0100

    Update Math.Between/Outside

--- WinCopies/Math/__init__.py
+++ WinCopies/Math/__init__.py
@@ -5,13 +5,10 @@ Created on Tue Jan 21 05:25:00 2025
 @author: Pierre Sprimont
 """
 
-def Between(x: int, value: int, y: int, bx: bool, by: bool) -> bool:
-    def check(x: int, y: int, b: bool) -> bool:
-        return x <= y if b else x < y
-    
-    return check(x, value, bx) and check(value, y, by)
-def Outside(x: int, value: int, y: int, bx: bool, by: bool) -> bool:
-    def check(x: int, y: int, b: bool) -> bool:
-        return y <= x if b else y < x
-    
-    return check(x, value, bx) or check(value, y, by)
+def __Check(x: int, y: int, b: bool) -> bool:
+    return x <= y if b else x < y
+
+def Between(x: int, value: int, y: int, bx: bool = True, by: bool = True) -> bool:
+    return __Check(x, value, bx) and __Check(value, y, by)
+def Outside(x: int, value: int, y: int, bx: bool = True, by: bool = True) -> bool:
+    return __Check(value, x, bx) or __Check(y, value, by)
