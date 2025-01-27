8e7e7f05eb73f8093aa1b5767edfce83936f18ba
pierresprim <49271108+pierresprim@users.noreply.github.com>
2025-01-27 11:10:59 +0100

    Remove unused imports

--- WinCopies/Collections/Linked/Enumeration.py
+++ WinCopies/Collections/Linked/Enumeration.py
@@ -1,5 +1,4 @@
-from abc import ABC, abstractmethod
-from typing import final, Self
+from typing import final
 
 from WinCopies.Collections import Generator
 from WinCopies.Collections.Enumeration import Enumerator
