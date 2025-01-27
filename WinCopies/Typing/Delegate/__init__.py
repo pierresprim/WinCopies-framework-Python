1f19894f3269cbaacf3347b41b7bd6ed96a0a8af
pierresprim <49271108+pierresprim@users.noreply.github.com>
2025-01-27 10:57:32 +0100

    Update Delegate module

--- WinCopies/Typing/Delegate/__init__.py
+++ WinCopies/Typing/Delegate/__init__.py
@@ -1,7 +1,8 @@
 from typing import Callable
 
-type Method = Callable[[], None]
+type Action = Callable[[], None]
+type Method[T] = Callable[[T], None]
 type Function[T] = Callable[[], T]
-type Predicate[T] = Callable[[T], bool]
 type Converter[TIn, TOut] = Callable[[TIn], TOut]
+type Predicate[T] = Converter[T, bool]
 type Selector[T] = Converter[T, T]
