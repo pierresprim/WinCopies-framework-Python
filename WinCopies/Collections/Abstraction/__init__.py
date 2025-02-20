f2b85ead543c02ae209a524c1f57b847b19bbca6
pierresprim <49271108+pierresprim@users.noreply.github.com>
2025-02-20 10:13:57 +0100

    Add new type

--- WinCopies/Collections/Abstraction/__init__.py
+++ WinCopies/Collections/Abstraction/__init__.py
@@ -1,13 +1,13 @@
 import collections.abc
-import typing
 
 from collections.abc import Iterator
 from typing import final
 
-from WinCopies.Collections import Extensions, ICountable, IndexOf
-from WinCopies.Collections.Enumeration import IIterable, ICountableIterable
+from WinCopies.Collections import Enumeration, Extensions, ICountable, IDictionary, IndexOf
+from WinCopies.Collections.Enumeration import IIterable, ICountableIterable, IEnumerator, EnumeratorBase
 from WinCopies.Typing import EnsureDirectModuleCall
 from WinCopies.Typing.Delegate import Predicate
+from WinCopies.Typing.Pairing import IKeyValuePair, KeyValuePair
 
 class Array[T](Extensions.Array[T]):
     def __init__(self, items: tuple[T, ...]|collections.abc.Iterable[T]):
@@ -86,6 +86,111 @@ class List[T](Extensions.List[T]):
     @final
     def Clear(self) -> None:
         self._GetList().clear()
+
+class Dictionary[TKey, TValue](IDictionary[TKey, TValue], IIterable[IKeyValuePair[TKey, TValue]]):
+    @final
+    class Enumerator(EnumeratorBase[IKeyValuePair[TKey, TValue]]):
+        @final
+        class KeyValuePair(IKeyValuePair[TKey, TValue]):
+            def __init__(self, item: tuple[TKey, TValue]):
+                self.__item: tuple[TKey, TValue] = item
+            
+            @final
+            def IsKeyValuePair(self) -> bool:
+                return True
+            
+            @final
+            def GetKey(self) -> TKey:
+                return self.__item[0]
+            @final
+            def GetValue(self) -> TValue:
+                return self.__item[1]
+    
+            @final
+            def _Equals(self, item: IKeyValuePair[TKey, TValue]) -> bool:
+                return isinstance(item, Dictionary.Enumerator.KeyValuePair)
+        
+        def __init__(self, dictionary: dict[TKey, TValue]):
+            super().__init__()
+
+            self.__dictionary: dict[TKey, TValue] = dictionary
+            self.__iterator: Enumeration.Iterator[tuple[TKey, TValue]]|None = None
+            self.__current: IKeyValuePair[TKey, TValue]|None = None
+        
+        def IsResetSupported(self) -> bool:
+            return True
+        
+        def _OnStarting(self) -> bool:
+            if super()._OnStarting():
+                self.__iterator = Enumeration.Iterator(self.__dictionary.items().__iter__())
+                
+                return True
+            
+            return False
+        
+        def _MoveNextOverride(self) -> bool:
+            if self.__iterator.MoveNext():
+                item: tuple[TKey, TValue] = self.__iterator.GetCurrent()
+
+                self.__current = Dictionary.Enumerator.KeyValuePair(item)
+
+                return True
+            
+            return False
+        
+        def GetCurrent(self) -> IKeyValuePair[TKey, TValue]:
+            return self.__current
+        
+        def _OnEnded(self) -> None:
+            self.__iterator = None
+            self.__current = None
+
+            super()._OnEnded()
+        
+        def _OnStopped(self) -> None:
+            pass
+        
+        def _ResetOverride(self) -> bool:
+            return True
+    
+    def __init__(self, dictionary: dict[TKey, TValue]|None = None):
+        super().__init__()
+
+        self.__dictionary: dict[TKey, TValue] = dict[TKey, TValue]() if dictionary is None else dictionary
+    
+    @final
+    def _GetDictionary(self) -> dict[TKey, TValue]:
+        return self.__dictionary
+    
+    @final
+    def GetCount(self) -> int:
+        return len(self._GetDictionary())
+    
+    @final
+    def GetAt(self, key: TKey) -> TValue:
+        return self._GetDictionary()[key]
+    @final
+    def SetAt(self, key: TKey, value: TValue) -> None:
+        self._GetDictionary()[key] = value
+    
+    @final
+    def Add(self, key: TKey, value: TValue) -> None:
+        self._GetDictionary()[key] = value
+    @final
+    def AddItem(self, item: KeyValuePair[TKey, TValue]) -> None:
+        self.Add(item.GetKey(), item.GetValue())
+
+    @final
+    def Remove(self, key: TKey) -> None:
+        self._GetDictionary().pop(key)
+    
+    @final
+    def Clear(self) -> None:
+        return self._GetDictionary().clear()
+    
+    @final
+    def TryGetIterator(self) -> IEnumerator[IKeyValuePair[TKey, TValue]]:
+        return Dictionary.Enumerator(self._GetDictionary())
 
 class Countable(ICountable):
     def __init__(self, collection: ICountable):
