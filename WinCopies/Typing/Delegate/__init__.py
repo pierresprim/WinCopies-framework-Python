e2db14828dd43cd47893a6f7f31e6eb732473b9b
pierresprim <49271108+pierresprim@users.noreply.github.com>
2025-03-01 01:05:13 +0100

    Add new types

--- WinCopies/Typing/Delegate/__init__.py
+++ WinCopies/Typing/Delegate/__init__.py
@@ -1,4 +1,5 @@
-from typing import Callable
+from abc import abstractmethod, ABC
+from typing import final, Callable
 
 type Action = Callable[[], None]
 type Method[T] = Callable[[T], None]
@@ -8,3 +9,50 @@ type Predicate[T] = Converter[T, bool]
 type EqualityComparison[T] = Callable[[T, T], bool]
 type IndexedValueComparison[T] = Callable[[int, T], bool]
 type Selector[T] = Converter[T, T]
+
+class IFunction[T](ABC):
+    def __init__(self):
+        super().__init__()
+
+    @abstractmethod
+    def GetValue(self) -> T:
+        pass
+
+@final
+class ValueFunction[T](IFunction[T]):
+    def __init__(self, value: T):
+        super().__init__()
+
+        self.__value: T = value
+    
+    def GetValue(self) -> T:
+        return self.__value
+
+class FunctionUpdater[T](IFunction[T]):
+    def __init__(self, updater: Method[IFunction[T]]):
+        super().__init__()
+
+        self.__updater: Method[IFunction[T]] = updater
+    
+    @abstractmethod
+    def _GetFunction(self) -> IFunction[T]:
+        pass
+    
+    @final
+    def GetValue(self) -> T:
+        function: IFunction[T] = self._GetFunction()
+
+        self.__updater(function)
+        
+        return function()
+class ValueFunctionUpdater[T](FunctionUpdater[T]):
+    def __init__(self, updater: Method[IFunction[T]]):
+        super().__init__(updater)
+    
+    @abstractmethod
+    def _GetValue(self) -> T:
+        pass
+    
+    @final
+    def _GetFunction(self) -> IFunction[T]:
+        return ValueFunction[T](self._GetValue())
