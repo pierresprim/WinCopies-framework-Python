caf2e8563d13c10fbae4c6d1b06d65e2f56f1be7
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2025-03-03 01:29:19 +0100

    Update the base interface of Collections.Abstraction.Dictionary

--- WinCopies/Collections/Abstraction/__init__.py
+++ WinCopies/Collections/Abstraction/__init__.py
@@ -90,7 +90,7 @@ class List[T](Extensions.List[T]):
     def Clear(self) -> None:
         self._GetList().clear()
 
-class Dictionary[TKey, TValue](IDictionary[TKey, TValue]):
+class Dictionary[TKey, TValue](Extensions.IDictionary[TKey, TValue]):
     @final
     class Enumerator(EnumeratorBase[IKeyValuePair[TKey, TValue]]):
         @final
