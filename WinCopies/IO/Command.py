7d6624e6b2e9cd844a05cbc6ec4469c86f39674a
pierresprim <49271108+pierresprim@users.noreply.github.com>
2025-08-19 11:34:23 +0200

    Update type hints

--- WinCopies/IO/Command.py
+++ WinCopies/IO/Command.py
@@ -7,11 +7,11 @@ from WinCopies import Typing
 from WinCopies.Collections import Iteration
 from WinCopies.Typing.Pairing import DualResult, KeyValuePair
 
-def Run(command: str|Iterable[str], captureOutput = False, shell = False, throwOnError = True) -> DualResult[object, int]:
+def Run(command: str|Iterable[str], captureOutput: bool = False, shell: bool = False, throwOnError: bool = True) -> DualResult[object, int]:
     args: str|list[str]|None = None
     
     if Typing.IsOf(command, str, list):
-        args = command
+        args = command # type: ignore
     
     elif Typing.IsOf(command, Iterable):
         args = list(command)
@@ -19,13 +19,13 @@ def Run(command: str|Iterable[str], captureOutput = False, shell = False, throwO
     else:
         raise ValueError(f"command must be a string, Iterable[str] or list[str]. Command is {type(command)}", command)
     
-    result: subprocess.CompletedProcess = subprocess.run(args, capture_output = captureOutput, shell = shell, text = captureOutput, check = throwOnError, stdout = None if captureOutput else sys.__stdout__)
+    result: subprocess.CompletedProcess[str] = subprocess.run(args, stdout = (None if captureOutput else sys.__stdout__), shell = shell, capture_output = captureOutput, check = throwOnError, text = captureOutput)
     
     return DualResult(result.stdout, result.returncode)
 
-def RunWithArgs(command: str, args: Iterable[str], captureOutput = False, shell = False, throwOnError = True) -> DualResult[object, int]:
+def RunWithArgs(command: str, args: Iterable[str], captureOutput: bool = False, shell: bool = False, throwOnError: bool = True) -> DualResult[object, int]:
     return Run(Iteration.PrependValues(args, command), captureOutput, shell, throwOnError)
-def RunWithArgValues(command: str, captureOutput, shell, throwOnError, *args: str) -> DualResult[object, int]:
+def RunWithArgValues(command: str, captureOutput: bool, shell: bool, throwOnError: bool, *args: str) -> DualResult[object, int]:
     return RunWithArgs(command, args, captureOutput, shell, throwOnError)
 
 def GetArgument(name: str, argument: str|None) -> str:
@@ -33,10 +33,10 @@ def GetArgument(name: str, argument: str|None) -> str:
 def TryGetArgument(name: str, argument: str|None) -> str|None:
     return None if argument is None else f"--{name} {argument}"
 
-def GetArgumentTuple(name: str, argument: str) -> tuple:
+def GetArgumentTuple(name: str, argument: str) -> tuple[str]:
     return (GetArgument(name, argument),)
 
-def GetArgumentPair(name: str, key: str, value: str) -> str:
+def GetArgumentPair(name: str, key: object, value: object) -> str:
     return GetArgument(name, f"{key}:{value}")
 def GetArgumentKeyValuePair[TKey, TValue](name: str, pair: KeyValuePair[TKey, TValue]) -> str:
     return GetArgumentPair(name, pair.GetKey(), pair.GetValue())
