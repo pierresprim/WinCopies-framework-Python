300d46a8c9382658188bf7cc40a79432051377f2
pierresprim <49271108+pierresprim@users.noreply.github.com>
2025-08-19 19:14:33 +0200

    Bug fixes

--- WinCopies/Collections/Extensions.py
+++ WinCopies/Collections/Extensions.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-from typing import final, Self
+from abc import abstractmethod
+from typing import final
 
 from WinCopies import Collections
 from WinCopies.Collections.Enumeration import IIterable, IEnumerator, EnumeratorBase
@@ -16,17 +17,19 @@ class IDictionary[TKey, TValue](Collections.IDictionary[TKey, TValue], IIterable
     def __init__(self):
         super().__init__()
 
-class ArrayBase[TItems, TList: IArray[TItems]](Collections.Array[TItems], IArray[TItems]):
+class ArrayBase[TItems, TList](Collections.Array[TItems], IArray[TItems]):
     @final
     class Enumerator(EnumeratorBase[TItems]):
-        def __init__(self, items: TList):
+        def __init__(self, items: ArrayBase[TItems, TList]):
             super().__init__()
 
-            self.__list: TList = items
+            self.__list: ArrayBase[TItems, TList] = items
             self.__i: int = -1
         
-        def _GetList(self) -> TList:
+        def _GetListAsArray(self) -> IArray[TItems]:
             return self.__list
+        def _GetList(self) -> TList:
+            return self.__list._AsList()
         
         def IsResetSupported(self) -> bool:
             return True
@@ -34,10 +37,10 @@ class ArrayBase[TItems, TList: IArray[TItems]](Collections.Array[TItems], IArray
         def _MoveNextOverride(self) -> bool:
             self.__i += 1
             
-            return self.__i < self._GetList().GetCount()
+            return self.__i < self._GetListAsArray().GetCount()
         
         def GetCurrent(self) -> TItems:
-            return self._GetList().GetAt(self.__i)
+            return self._GetListAsArray().GetAt(self.__i)
         
         def _OnStopped(self) -> None:
             pass
@@ -50,14 +53,26 @@ class ArrayBase[TItems, TList: IArray[TItems]](Collections.Array[TItems], IArray
     def __init__(self):
         super().__init__()
     
-    @final
+    @abstractmethod
+    def _AsList(self) -> TList:
+        pass
+    
+    # Not final to allow customization of the enumerator.
     def TryGetIterator(self) -> IEnumerator[TItems]:
-        return ArrayBase.Enumerator(self)
+        return ArrayBase[TItems, TList].Enumerator(self)
 
-class Array[T](ArrayBase[T, Self]):
+class Array[T](ArrayBase[T, 'Array']):
     def __init__(self):
         super().__init__()
+    
+    @final
+    def _AsList(self) -> Array[T]:
+        return self
 
-class List[T](Collections.List[T], ArrayBase[T, Self], IList[T]):
+class List[T](Collections.List[T], ArrayBase[T, 'List'], IList[T]):
     def __init__(self):
         super().__init__()
+    
+    @final
+    def _AsList(self) -> List[T]:
+        return self
