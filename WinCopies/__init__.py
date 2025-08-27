07a9c5624ce53e53e90bf4428690d5e63ab778d3
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2025-08-27 06:18:34 +0200

    Split __init__.py

--- WinCopies/__init__.py
+++ WinCopies/__init__.py
@@ -10,9 +10,7 @@ from __future__ import annotations
 from abc import abstractmethod, ABC
 from enum import Enum
 from types import TracebackType
-from typing import final, Callable, Self
-
-from WinCopies.Typing.Delegate import Action, Predicate
+from typing import final, Self
 
 class IBooleanable(ABC):
     def __init__(self):
@@ -133,70 +131,5 @@ def ReadInt(message: str, errorMessage: str = "Invalid value; an integer is expe
     
     return value
 
-def AskInt(message: str, predicate: Predicate[int], errorMessage: str = "The value is out of range."):
-    value: int = 0
-    
-    def loop() -> int:
-        return ReadInt(message)
-    
-    value = loop()
-    
-    while predicate(value):
-        print(errorMessage)
-        
-        value = loop()
-    
-    return value
-
 def AskConfirmation(message: str, info: str = " [y]/any other key: ", value: str = "y") -> bool:
     return input(message + info) == value
-
-def Process(action: Action, message: str = "Continue?", info: str = " [y]/any other key: ", value: str = "y"):
-    while AskConfirmation(message, info, value):
-        action()
-
-def DoProcess(action: Action, message: str = "Continue?", info: str = " [y]/any other key: ", value: str = "y"):
-    action()
-    
-    Process(action, message, info, value)
-
-def TryPredicate(predicate: Predicate[Exception], action: Action) -> bool|None:
-    ok: bool = True
-    _predicate: Predicate[Exception]
-
-    def __predicate(e: Exception) -> bool:
-        nonlocal ok
-        nonlocal _predicate
-
-        if predicate(e):
-            ok = False
-            _predicate = predicate
-
-            return True
-        
-        return False
-    
-    _predicate = __predicate
-    
-    while True:
-        try:
-            action()
-
-        except Exception as e:
-            if _predicate(e):
-                continue
-                
-            return None
-        
-        break
-
-    return ok
-def Try(action: Action, onError: Callable[[Exception], None], func: Callable[[], bool]) -> bool|None:
-    def _onError(e: Exception) -> bool:
-        onError(e)
-        
-        return func()
-    
-    return TryPredicate(_onError, action)
-def TryMessage(action: Action, onError: Callable[[Exception], None], message: str = "Continue?") -> bool|None:
-    return Try(action, onError, lambda: AskConfirmation(message))
