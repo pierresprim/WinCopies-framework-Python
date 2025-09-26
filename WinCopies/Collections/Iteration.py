2ad9a9f1473e38d8813b4139af13a3c8d2949a49
pierresprim <49271108+pierresprim@users.noreply.github.com>
2025-09-26 23:19:17 +0200

    Update iteration functions

--- WinCopies/Collections/Iteration.py
+++ WinCopies/Collections/Iteration.py
@@ -1,124 +1,164 @@
 from collections.abc import Iterable, Iterator
 
 from WinCopies.Collections import Generator, Enumeration
-from WinCopies.Collections.Enumeration import IEnumerator, GetEmptyEnumerator, AsEnumerator
+from WinCopies.Collections.Enumeration import IEnumerable, IEnumerator
 from WinCopies.Collections.Enumeration.Selection import ExcluerEnumerator, ExcluerUntilEnumerator
 from WinCopies.Delegates import GetNotPredicate
 from WinCopies.Typing import INullable, GetNullable, GetNullValue
 from WinCopies.Typing.Delegate import Converter, Predicate
 
-def Append[T](items: Iterable[T], values: Iterable[T]) -> Generator[T]:
-    for item in items:
+def TryEnumerate[T](iterable: Iterable[T]|None) -> Iterable[T]:
+    def getEmptyGenerator(*values: T) -> Generator[T]:
+        yield from values
+
+    return getEmptyGenerator() if iterable is None else iterable
+
+def Concatenate[T](collection: Iterable[Iterable[T]|None]|None) -> Generator[T]:
+    for iterable in TryEnumerate(collection):
+        for item in TryEnumerate(iterable):
+            yield item
+
+def Append[T](items: Iterable[T]|None, values: Iterable[T]|None) -> Generator[T]:
+    for item in TryEnumerate(items):
         yield item
     
-    for value in values:
+    for value in TryEnumerate(values):
         yield value
-def AppendValues[T](items: Iterable[T], *values: T) -> Generator[T]:
+def AppendValues[T](items: Iterable[T]|None, *values: T) -> Generator[T]:
     return Append(items, values)
 
-def Prepend[T](items: Iterable[T], values: Iterable[T]) -> Generator[T]:
+def Prepend[T](items: Iterable[T]|None, values: Iterable[T]|None) -> Generator[T]:
     return Append(values, items)
-def PrependValues[T](items: Iterable[T], *values: T) -> Generator[T]:
+def PrependValues[T](items: Iterable[T]|None, *values: T) -> Generator[T]:
     return Prepend(items, values)
 
-def AppendTo[T](items: Iterable[T], values: Iterable[T]) -> Generator[T]:
-    for item in items:
+def AppendTo[T](items: Iterable[T]|None, values: Iterable[T]|None) -> Generator[T]:
+    for item in TryEnumerate(items):
         yield item
 
-        for value in values:
+        for value in TryEnumerate(values):
             yield value
-def AppendValuesTo[T](items: Iterable[T], *values: T) -> Generator[T]:
+def AppendValuesTo[T](items: Iterable[T]|None, *values: T) -> Generator[T]:
     return AppendTo(items, values)
 
-def PrependTo[T](items: Iterable[T], values: Iterable[T]) -> Generator[T]:
-    for item in items:
-        for value in values:
+def PrependTo[T](items: Iterable[T]|None, values: Iterable[T]|None) -> Generator[T]:
+    for item in TryEnumerate(items):
+        for value in TryEnumerate(values):
             yield value
         
         yield item
-def PrependValuesTo[T](items: Iterable[T], *values: T) -> Generator[T]:
+def PrependValuesTo[T](items: Iterable[T]|None, *values: T) -> Generator[T]:
     return PrependTo(items, values)
 
-def AppendItemTo[T](items: Iterable[T], value: T) -> Generator[T]:
-    for item in items:
+def AppendItemTo[T](items: Iterable[T]|None, value: T) -> Generator[T]:
+    for item in TryEnumerate(items):
         yield item
 
         yield value
-def PrependItemTo[T](items: Iterable[T], value: T) -> Generator[T]:
-    for item in items:
+def PrependItemTo[T](items: Iterable[T]|None, value: T) -> Generator[T]:
+    for item in TryEnumerate(items):
         yield value
 
         yield item
 
-def Select[TIn, TOut](items: Iterable[TIn], converter: Converter[TIn, TOut]) -> Generator[TOut]:
-    return (converter(item) for item in items)
-def WhereSelect[TIn, TOut](items: Iterable[TIn], predicate: Predicate[TIn], converter: Converter[TIn, TOut]) -> Generator[TOut]:
-    return (converter(item) for item in items if predicate(item))
-def SelectWhere[TIn, TOut](items: Iterable[TIn], converter: Converter[TIn, TOut], predicate: Predicate[TOut]) -> Generator[TOut]:
+def AppendIterable[T](items: Iterable[T]|None, values: Iterable[Iterable[T]|None]|None) -> Generator[T]:
+    return Append(items, Concatenate(values))
+def AppendIterableValues[T](items: Iterable[T]|None, *values: Iterable[T]|None) -> Generator[T]:
+    return AppendIterable(items, values)
+
+def PrependIterable[T](items: Iterable[T]|None, values: Iterable[Iterable[T]|None]|None) -> Generator[T]:
+    return Append(Concatenate(values), items)
+def PrependIterableValues[T](items: Iterable[T]|None, *values: Iterable[T]|None) -> Generator[T]:
+    return PrependIterable(items, values)
+
+def AppendIterableTo[T](items: Iterable[T]|None, values: Iterable[Iterable[T]|None]|None) -> Generator[T]:
+    return AppendTo(items, Concatenate(values))
+def AppendIterableValuesTo[T](items: Iterable[T]|None, *values: Iterable[T]|None) -> Generator[T]:
+    return AppendIterableTo(items, values)
+
+def PrependIterableTo[T](items: Iterable[T]|None, values: Iterable[Iterable[T]|None]|None) -> Generator[T]:
+    return PrependTo(items, Concatenate(values))
+def PrependIterableValuesTo[T](items: Iterable[T]|None, *values: Iterable[T]|None) -> Generator[T]:
+    return PrependIterableTo(items, values)
+
+def Select[TIn, TOut](items: Iterable[TIn]|None, converter: Converter[TIn, TOut]) -> Generator[TOut]:
+    return (converter(item) for item in TryEnumerate(items))
+def WhereSelect[TIn, TOut](items: Iterable[TIn]|None, predicate: Predicate[TIn], converter: Converter[TIn, TOut]) -> Generator[TOut]:
+    return (converter(item) for item in TryEnumerate(items) if predicate(item))
+def SelectWhere[TIn, TOut](items: Iterable[TIn]|None, converter: Converter[TIn, TOut], predicate: Predicate[TOut]) -> Generator[TOut]:
     result: TOut|None = None
 
-    for item in items:
+    for item in TryEnumerate(items):
         if predicate(result := converter(item)):
             yield result
 
-def Include[T](items: Iterable[T], predicate: Predicate[T]) -> Generator[T]:
-    return (item for item in items if predicate(item))
-def Exclude[T](items: Iterable[T], predicate: Predicate[T]) -> Generator[T]:
+def Include[T](items: Iterable[T]|None, predicate: Predicate[T]) -> Generator[T]:
+    return (item for item in TryEnumerate(items) if predicate(item))
+def Exclude[T](items: Iterable[T]|None, predicate: Predicate[T]) -> Generator[T]:
     return Include(items, GetNotPredicate(predicate))
 
-def IncludeWhile[T](items: Iterable[T], predicate: Predicate[T]) -> Generator[T]:
-    for item in items:
+def IncludeWhile[T](items: Iterable[T]|None, predicate: Predicate[T]) -> Generator[T]:
+    for item in TryEnumerate(items):
         if predicate(item):
             yield item
         
         else:
             break
-def IncludeUntil[T](items: Iterable[T], predicate: Predicate[T]) -> Generator[T]:
-    for item in items:
+def IncludeUntil[T](items: Iterable[T]|None, predicate: Predicate[T]) -> Generator[T]:
+    for item in TryEnumerate(items):
         if predicate(item):
             break
         
         yield item
 
-def DoIncludeUntil[T](items: Iterable[T], predicate: Predicate[T]) -> Generator[T]:
-    for item in items:
+def DoIncludeUntil[T](items: Iterable[T]|None, predicate: Predicate[T]) -> Generator[T]:
+    for item in TryEnumerate(items):
         yield item
 
         if predicate(item):
             break
-def DoIncludeWhile[T](items: Iterable[T], predicate: Predicate[T]) -> Generator[T]:
+def DoIncludeWhile[T](items: Iterable[T]|None, predicate: Predicate[T]) -> Generator[T]:
     return DoIncludeUntil(items, GetNotPredicate(predicate))
 
-def __Exclude[T](items: Iterable[T], converter: Converter[Iterator[T], IEnumerator[T]]) -> Generator[T]:
-    def getEnumerator(iterator: Iterator[T]|None) -> IEnumerator[T]:
-        return GetEmptyEnumerator() if iterator is None else converter(iterator)
+def __Exclude[T](items: Iterable[T]|None, converter: Converter[IEnumerator[T], IEnumerator[T]]) -> Generator[T]:
+    def getEnumerator(enumerable: IEnumerable[T]|None) -> Iterator[T]|None:
+        if enumerable is None:
+            return None
+        
+        enumerator: IEnumerator[T]|None = enumerable.TryGetEnumerator()
+        
+        return None if enumerator is None else converter(enumerator).AsIterator()
     
-    for item in getEnumerator(Enumeration.Iterable[T].Create(items).TryGetIterator()):
+    for item in TryEnumerate(getEnumerator(Enumeration.Iterable[T].TryCreate(items))):
         yield item
 
-def ExcludeWhile[T](items: Iterable[T], predicate: Predicate[T]) -> Generator[T]:
-    return __Exclude(items, lambda iterator: ExcluerEnumerator(AsEnumerator(iterator), predicate))
-def ExcludeUntil[T](items: Iterable[T], predicate: Predicate[T]) -> Generator[T]:
-    return __Exclude(items, lambda iterator: ExcluerUntilEnumerator(AsEnumerator(iterator), predicate))
+def ExcludeWhile[T](items: Iterable[T]|None, predicate: Predicate[T]) -> Generator[T]:
+    return __Exclude(items, lambda iterator: ExcluerEnumerator(iterator, predicate))
+def ExcludeUntil[T](items: Iterable[T]|None, predicate: Predicate[T]) -> Generator[T]:
+    return __Exclude(items, lambda iterator: ExcluerUntilEnumerator(iterator, predicate))
 
-def Concatenate[T](collection: Iterable[Iterable[T]]) -> Generator[T]:
-    for iterable in collection:
-        for item in iterable:
-            yield item
-
-def TryGetFirst[T](items: Iterable[T]) -> INullable[T]:
+def TryGetFirst[T](items: Iterable[T]|None) -> INullable[T]|None:
+    if items is None:
+        return None
+    
     for item in items:
         return GetNullable(item)
     
     return GetNullValue()
 
-def Any[T](items: Iterable[T]) -> bool:
+def Any[T](items: Iterable[T]|None) -> bool|None:
+    if items is None:
+        return None
+    
     for _ in items:
         return True
     
     return False
 
-def ValidateOnlyOne[T](items: Iterable[T], predicate: Predicate[T]) -> bool:
+def ValidateOnlyOne[T](items: Iterable[T]|None, predicate: Predicate[T]) -> bool|None:
+    if items is None:
+        return None
+    
     validator: Predicate[T]|None = None
 
     def validate(value: T) -> bool:
@@ -137,6 +177,6 @@ def ValidateOnlyOne[T](items: Iterable[T], predicate: Predicate[T]) -> bool:
             return False
     
     return True # Validation succeeded.
-def EnsureOnlyOne[T](items: Iterable[T], predicate: Predicate[T], errorMessage: str|None = None) -> None:
+def EnsureOnlyOne[T](items: Iterable[T]|None, predicate: Predicate[T], errorMessage: str|None = None) -> None:
     if not ValidateOnlyOne(items, predicate):
         raise ValueError("More than one value validating the given predicate were found." if errorMessage is None else errorMessage)
