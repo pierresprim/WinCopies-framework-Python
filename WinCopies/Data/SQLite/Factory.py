b1748f93ecf239e76efdf7152adbba40a8110941
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2025-09-30 16:53:40 +0200

    Add new type

--- WinCopies/Data/SQLite/Factory.py
+++ WinCopies/Data/SQLite/Factory.py
@@ -1,3 +1,4 @@
+from abc import abstractmethod
 from collections.abc import Iterable
 from enum import Enum
 from typing import final
@@ -10,16 +11,21 @@ import WinCopies.Data
 
 from WinCopies import String
 from WinCopies.Collections.Enumeration import ICountableEnumerable
-from WinCopies.Collections.Extensions import IDictionary
+from WinCopies.Collections.Extensions import IEquatableTuple, IDictionary
+from WinCopies.Collections.Iteration import Select
 from WinCopies.Enum import HasFlag
-from WinCopies.Typing import IString
+from WinCopies.String import CommaJoin
+from WinCopies.Typing import InvalidOperationError, IString
+from WinCopies.Typing.Pairing import DualResult
 from WinCopies.Typing.Reflection import EnsureDirectPackageCall, EnsureCallerPackage
 
 
 
 from WinCopies.Data import Field
-from WinCopies.Data.Factory import IFieldFactory, IQueryFactory
+from WinCopies.Data.Abstract import IConnection
+from WinCopies.Data.Factory import IFieldFactory, IQueryFactory, IIndexFactory
 from WinCopies.Data.Field import FieldAttributes, IntegerMode, RealMode, TextMode, IField
+from WinCopies.Data.Index import IndexType, IIndex, IKey, ISingleColumnIndex, IMultiColumnIndex, IMultiColumnKey, IForeignKey, UnicityIndex, PrimaryKey, ForeignKey
 from WinCopies.Data.Parameter import IParameter
 from WinCopies.Data.Query import ISelectionQuery, IInsertionQuery, IMultiInsertionQuery, IUpdateQuery
 from WinCopies.Data.Set import ITableParameterSet, IColumnParameterSet
@@ -135,3 +141,101 @@ class QueryFactory(IQueryFactory):
     
     def GetUpdateQuery(self, tableName: str, values: IDictionary[IString, object], conditions: IConditionParameterSet|None) -> IUpdateQuery:
         return UpdateQuery(self._GetConnection(), tableName, values, conditions)
+
+@final
+class IndexFactory(IIndexFactory):
+    class _Index(IIndex):
+        def __init__(self):
+            EnsureCallerPackage(WinCopies.Data)
+
+            super().__init__()
+
+        @abstractmethod
+        def _GetConnection(self) -> IConnection:
+            pass
+        
+        @abstractmethod
+        def _GetStringType(self) -> str:
+            pass
+
+        @final
+        def _FormatTableName(self, name: str) -> str:
+            return self._GetConnection().FormatTableName(name)
+        
+        @final
+        def _GetHeader(self) -> str:
+            return f"CONSTRAINT {self._FormatTableName(self.GetName())} {self._GetStringType()}"
+    class _MultiColumnIndex(_Index, IMultiColumnIndex):
+        def __init__(self):
+            super().__init__()
+        
+        @final
+        def _GetStringColumns(self) -> str:
+            return f"({CommaJoin(Select(self.GetColumns().AsIterable(), lambda columnName: self._FormatTableName(columnName.ToString())))})"
+        
+        def ToString(self) -> str:
+            return f"{self._GetHeader()} {self._GetStringColumns()}"
+    @final
+    class __UnicityIndex(UnicityIndex, _MultiColumnIndex):
+        def __init__(self, name: str, columns: IEquatableTuple[IString]|Iterable[IString], connection: IConnection):
+            super().__init__(name, columns)
+
+            self.__connection: IConnection = connection
+        
+        def _GetConnection(self) -> IConnection:
+            return self.__connection
+        
+        def _GetStringType(self) -> str:
+            return IndexType.Unique.name.upper()
+    class _Key(_Index, IKey):
+        def __init__(self):
+            super().__init__()
+        
+        def _GetStringType(self) -> str:
+            return f"{self.GetKeyType().name.upper()} {IndexType.Key.name.upper()}"
+    @final
+    class __PrimaryKey(PrimaryKey, _MultiColumnIndex, _Key):
+        def __init__(self, name: str, columns: IEquatableTuple[IString]|Iterable[IString], connection: IConnection):
+            super().__init__(name, columns)
+
+            self.__connection: IConnection = connection
+        
+        def _GetConnection(self) -> IConnection:
+            return self.__connection
+    @final
+    class __ForeignKey(ForeignKey, _Key):
+        def __init__(self, name: str, column: str, foreignKey: DualResult[str, str], connection: IConnection):
+            super().__init__(name, column, foreignKey)
+
+            self.__connection: IConnection = connection
+        
+        def _GetConnection(self) -> IConnection:
+            return self.__connection
+        
+        def ToString(self) -> str:
+            foreignKey: DualResult[str, str] = self.GetForeignKey()
+
+            return f"{super().ToString()} REFERENCES {self._FormatTableName(foreignKey.GetKey())} ({self._FormatTableName(foreignKey.GetValue())})"
+    
+    def __init__(self, connection: IConnection):
+        EnsureDirectPackageCall()
+        
+        super().__init__()
+
+        self.__connection: IConnection = connection
+    
+    def _GetConnection(self) -> IConnection:
+        return self.__connection
+    
+    @final
+    def GetPrimaryKey(self, name: str, columns: IEquatableTuple[IString]|Iterable[IString]) -> IMultiColumnKey:
+        return IndexFactory.__PrimaryKey(name, columns, self._GetConnection())
+    @final
+    def GetForeignKey(self, name: str, column: str, foreignKey: DualResult[str, str]) -> IForeignKey:
+        return IndexFactory.__ForeignKey(name, column, foreignKey, self._GetConnection())
+    @final
+    def GetNormalIndex(self, name: str, column: str) -> ISingleColumnIndex:
+        raise InvalidOperationError("Not supported.")
+    @final
+    def GetUnicityIndex(self, name: str, columns: IEquatableTuple[IString]|Iterable[IString]) -> IMultiColumnIndex:
+        return IndexFactory.__UnicityIndex(name, columns, self._GetConnection())
