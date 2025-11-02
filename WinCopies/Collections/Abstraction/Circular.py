362f2db52df2f2dc5aeff0d5944d36e413aedd8a
pierresprim <49271108+pierresprim@users.noreply.github.com>
2025-11-02 05:48:09 +0100

    Add method implementations

--- WinCopies/Collections/Abstraction/Circular.py
+++ WinCopies/Collections/Abstraction/Circular.py
@@ -1,8 +1,11 @@
-from typing import final
+from collections.abc import Iterable, Sequence as SequenceBase
+from typing import overload, final, SupportsIndex
 
 from WinCopies.Collections.Circular import ICircularTuple, ICircularEquatableTuple, ICircularArray, ICircularList
-from WinCopies.Collections.Extensions import TupleBase, ArrayBase, Sequence, MutableSequence, Tuple, EquatableTuple
+from WinCopies.Collections.Extensions import ITuple, IEquatableTuple, IArray, IList, TupleBase, ArrayBase, Sequence, MutableSequence, Tuple, EquatableTuple, ReversedListBase
+from WinCopies.Collections.Range import GetItems, SetItems, RemoveItems
 from WinCopies.Typing import GenericConstraint, GenericSpecializedConstraint, IGenericConstraintImplementation, IGenericSpecializedConstraintImplementation, IEquatableItem
+from WinCopies.Typing.Delegate import IFunction, Method, ValueFunctionUpdater
 
 class CircularBase[TItem, TList](TupleBase[TItem], Sequence[TItem], ICircularTuple[TItem], GenericConstraint[TList, ICircularTuple[TItem]]):
     def __init__(self, items: TList):
@@ -19,6 +22,10 @@ class CircularBase[TItem, TList](TupleBase[TItem], Sequence[TItem], ICircularTup
         return self._GetInnerContainer().GetAt(key)
     
     @final
+    def Contains(self, value: TItem|object) -> bool:
+        return self._GetInnerContainer().Contains(value)
+    
+    @final
     def GetCount(self) -> int:
         return self._GetInnerContainer().GetCount()
     
@@ -28,13 +35,30 @@ class CircularBase[TItem, TList](TupleBase[TItem], Sequence[TItem], ICircularTup
     
     def ToString(self) -> str:
         return self._GetInnerContainer().ToString()
+    
+    @overload
+    def __getitem__(self, index: SupportsIndex) -> TItem: ...
+    @overload
+    def __getitem__(self, index: slice) -> SequenceBase[TItem]: ...
+    
+    @final
+    def __getitem__(self, index: SupportsIndex|slice) -> TItem|SequenceBase[TItem]:
+        return GetItems(self, index)
 
 class CircularTuple[T](CircularBase[T, ICircularTuple[T]], Tuple[T], IGenericConstraintImplementation[ICircularTuple[T]]):
     def __init__(self, items: ICircularTuple[T]):
         super().__init__(items)
+    
+    @final
+    def SliceAt(self, key: slice) -> ITuple[T]:
+        return self._GetInnerContainer().SliceAt(key)
 class CircularEquatableTuple[T: IEquatableItem](CircularBase[T, ICircularEquatableTuple[T]], EquatableTuple[T], ICircularEquatableTuple[T], IGenericConstraintImplementation[ICircularEquatableTuple[T]]):
     def __init__(self, items: ICircularEquatableTuple[T]):
         super().__init__(items)
+    
+    @final
+    def SliceAt(self, key: slice) -> IEquatableTuple[T]:
+        return self._GetContainer().SliceAt(key)
     
     def Hash(self) -> int:
         return self._GetContainer().Hash()
@@ -50,11 +74,74 @@ class CircularArrayBase[TItem, TList](CircularBase[TItem, TList], ArrayBase[TIte
     def _SetAt(self, key: int, value: TItem) -> None:
         self._GetSpecializedContainer().SetAt(key, value)
 class CircularArray[T](CircularArrayBase[T, ICircularArray[T]], IGenericSpecializedConstraintImplementation[ICircularTuple[T], ICircularArray[T]]):
+    @final
+    class __Reversed(ArrayBase[T, ICircularArray[T]].Reversed, ICircularArray[T], IGenericSpecializedConstraintImplementation[ITuple[T], ICircularArray[T]]):
+        def __init__(self, items: ICircularArray[T]):
+            super().__init__(items)
+        
+        def GetStart(self) -> int:
+            return self._GetContainer().GetStart()
+    
+    @final
+    class __Updater(ValueFunctionUpdater[ICircularArray[T]]):
+        def __init__(self, array: ICircularArray[T], updater: Method[IFunction[ICircularArray[T]]]):
+            super().__init__(updater)
+
+            self.__array: ICircularArray[T] = array
+        
+        def _GetValue(self) -> ICircularArray[T]:
+            return CircularArray[T].__Reversed(self.__array)
+    
     def __init__(self, items: ICircularArray[T]):
         super().__init__(items)
+    
+    @final
+    def _GetUpdater(self, func: Method[IFunction[ICircularArray[T]]]) -> IFunction[ICircularArray[T]]:
+        return CircularArray[T].__Updater(self, func)
+    
+    @final
+    def AsReversed(self) -> IArray[T]:
+        return self._AsReversed()
+    
+    @final
+    def SliceAt(self, key: slice) -> IArray[T]:
+        return self._GetContainer().SliceAt(key)
 class CircularList[T](CircularArrayBase[T, ICircularList[T]], MutableSequence[T], ICircularList[T], IGenericSpecializedConstraintImplementation[ICircularTuple[T], ICircularList[T]]):
+    @final
+    class __Reversed(ReversedListBase[T, ICircularList[T]], ICircularList[T], IGenericSpecializedConstraintImplementation[ITuple[T], ICircularList[T]]):
+        def __init__(self, items: ICircularList[T]):
+            super().__init__(items)
+        
+        def GetStart(self) -> int:
+            return self._GetContainer().GetStart()
+        
+        def _GetContainerAsList(self, container: ICircularList[T]) -> IList[T]:
+            return container
+    
+    @final
+    class __Updater(ValueFunctionUpdater[ICircularList[T]]):
+        def __init__(self, array: ICircularList[T], updater: Method[IFunction[ICircularList[T]]]):
+            super().__init__(updater)
+
+            self.__array: ICircularList[T] = array
+        
+        def _GetValue(self) -> ICircularList[T]:
+            return CircularList[T].__Reversed(self.__array)
+    
     def __init__(self, items: ICircularList[T]):
         super().__init__(items)
+    
+    @final
+    def _GetUpdater(self, func: Method[IFunction[ICircularList[T]]]) -> IFunction[ICircularList[T]]:
+        return CircularList[T].__Updater(self, func)
+    
+    @final
+    def AsReversed(self) -> IList[T]:
+        return self._AsReversed()
+    
+    @final
+    def SliceAt(self, key: slice) -> IList[T]:
+        return self._GetContainer().SliceAt(key)
     
     @final
     def Add(self, item: T) -> None:
@@ -71,3 +158,20 @@ class CircularList[T](CircularArrayBase[T, ICircularList[T]], MutableSequence[T]
     @final
     def Clear(self) -> None:
         self._GetContainer().Clear()
+    
+    @final
+    def insert(self, index: int, value: T) -> None:
+        self.Insert(index, value)
+    
+    @overload
+    def __setitem__(self, index: SupportsIndex, value: T) -> None: ...
+    @overload
+    def __setitem__(self, index: slice, value: Iterable[T]) -> None: ...
+    
+    @final
+    def __setitem__(self, index: SupportsIndex|slice, value: T|Iterable[T]) -> None:
+        SetItems(self, index, value)
+    
+    @final
+    def __delitem__(self, index: int|slice):
+        RemoveItems(self, index)
