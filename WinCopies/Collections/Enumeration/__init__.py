a63a149761df252e86165e01845767d7c9f5bec2
pierresprim <49271108+pierresprim@users.noreply.github.com>
2026-02-10 14:33:50 +0100

    Bug fix

--- WinCopies/Collections/Enumeration/__init__.py
+++ WinCopies/Collections/Enumeration/__init__.py
@@ -5,6 +5,8 @@ Created on Sun Feb 6 20:37:51 2022
 @author: Pierre Sprimont
 """
 
+from __future__ import annotations
+
 from abc import abstractmethod
 from collections.abc import Iterable as SystemIterable, Iterator as SystemIterator, Sized
 from typing import final
