6a239ddef300f2eb35c2a3b841108ec509adfa63
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2026-03-16 10:33:53 +0100

    Add new types

--- WinCopies/Data/Set/__init__.py
+++ WinCopies/Data/Set/__init__.py
@@ -1,14 +1,65 @@
+from __future__ import annotations
+
+from abc import abstractmethod
+from typing import final
+
+
+
+from WinCopies import IInterface
+from WinCopies.Collections.Enumeration import IEnumerable
 from WinCopies.Collections.Enumeration.Recursive import IRecursivelyEnumerable
 from WinCopies.Collections.Expression import ICompositeExpression
 from WinCopies.Collections.Extensions import IDictionary
 
+from WinCopies.Typing import INullable
 from WinCopies.Typing.Object import IString
-from WinCopies.Typing.Pairing import IKeyValuePair
+from WinCopies.Typing.Pairing import IKeyValuePair, TryGetKey, TryGetValue
 
 
 
 from WinCopies.Data import ConditionalOperator, IColumn, IOperandValue
 from WinCopies.Data.Parameter import IParameter, ITableParameter
+
+class IFieldParameterSetItem[TColumn: IColumn, TParameter: IParameter[IOperandValue]](IInterface):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    @abstractmethod
+    def TryGetFieldParameter(self) -> IKeyValuePair[TColumn, TParameter|None]|None:
+        pass
+    
+    @final
+    def TryGetColumn(self) -> INullable[TColumn]:
+        return TryGetKey(self.TryGetFieldParameter())
+    @final
+    def TryGetParameter(self) -> INullable[TParameter|None]:
+        return TryGetValue(self.TryGetFieldParameter())
+    
+    @abstractmethod
+    def TryGetItems(self) -> IEnumerable[IFieldParameterSetItem[TColumn, TParameter]]|None:
+        pass
+
+    @abstractmethod
+    def TryGetPreviousOperator(self) -> ConditionalOperator|None:
+        pass
+    @abstractmethod
+    def TryGetNextOperator(self) -> ConditionalOperator|None:
+        pass
+class IFieldConditionSetItem[T: IColumn](IFieldParameterSetItem[T, IParameter[IOperandValue]]):
+    def __init__(self) -> None:
+        super().__init__()
+
+class IFieldParameterRecursivelyEnumerable[TColumn: IColumn, TParameter: IParameter[IOperandValue]](IRecursivelyEnumerable[IFieldParameterSetItem[TColumn, TParameter]]):
+    def __init__(self) -> None:
+        super().__init__()
+class IFieldConditionRecursivelyEnumerable[T: IColumn](IFieldParameterRecursivelyEnumerable[T, IParameter[IOperandValue]]):
+    def __init__(self) -> None:
+        super().__init__()
+
+type IFieldConditionSetItemAlias[T: IColumn] = IFieldParameterSetItem[T, IParameter[IOperandValue]]
+
+type IFieldParameterRecursivelyEnumerableAlias[TColumn: IColumn, TParameter: IParameter[IOperandValue]] = IRecursivelyEnumerable[IFieldParameterSetItem[TColumn, TParameter]]
+type IFieldConditionRecursivelyEnumerableAlias[T: IColumn] = IFieldParameterRecursivelyEnumerableAlias[T, IParameter[IOperandValue]]
 
 class IParameterSet[T](IDictionary[IColumn, T]):
     def __init__(self) -> None:
