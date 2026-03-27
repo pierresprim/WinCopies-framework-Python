241b1760af8661946044b8a2d639763768c170fa
pierresprim <49271108+pierresprim@users.noreply.github.com>
2026-03-27 14:36:36 +0100

    Add new type alias and update math functions

--- WinCopies/Math/__init__.py
+++ WinCopies/Math/__init__.py
@@ -5,15 +5,19 @@ Created on Tue Jan 21 05:25:00 2025
 @author: Pierre Sprimont
 """
 
-def __Check(x: int, y: int, b: bool) -> bool:
+from decimal import Decimal as decimal
+
+type NumericalValue = int|float|decimal
+
+def __Check(x: NumericalValue, y: NumericalValue, b: bool) -> bool:
     return x <= y if b else x < y
 
-def Between(x: int, value: int, y: int, bx: bool = True, by: bool = True) -> bool:
+def Between(x: NumericalValue, value: NumericalValue, y: NumericalValue, bx: bool = True, by: bool = True) -> bool:
     return __Check(x, value, bx) and __Check(value, y, by)
-def Outside(x: int, value: int, y: int, bx: bool = True, by: bool = True) -> bool:
+def Outside(x: NumericalValue, value: NumericalValue, y: NumericalValue, bx: bool = True, by: bool = True) -> bool:
     return __Check(value, x, bx) or __Check(y, value, by)
 
-def CompareFrom(x: int, y: int) -> bool|None:
+def CompareFrom(x: NumericalValue, y: NumericalValue) -> bool|None:
     return None if x == y else x < y
-def CompareTo(x: int, y: int) -> bool|None:
+def CompareTo(x: NumericalValue, y: NumericalValue) -> bool|None:
     return None if x == y else x > y
