53f1f8632dd573e0bd670d5611e42a6af7d04a6d
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2026-05-11 02:47:18 +0200

    Add new protocol

--- WinCopies/Typing/Protocols.py
+++ WinCopies/Typing/Protocols.py
@@ -28,3 +28,27 @@ class SupportsRichComparison(Protocol):
     def __ge__(self, other: Any, /) -> bool:
         """Greater than or equal comparison."""
         ...
+
+@runtime_checkable
+class SupportsEqualityAndRichComparison(Protocol):
+    def __eq__(self, other: Any, /) -> bool:
+        ...
+    
+    def __hash__(self, /) -> int:
+        ...
+    
+    def __lt__(self, other: Any, /) -> bool:
+        """Less than comparison."""
+        ...
+    
+    def __le__(self, other: Any, /) -> bool:
+        """Less than or equal comparison."""
+        ...
+    
+    def __gt__(self, other: Any, /) -> bool:
+        """Greater than comparison."""
+        ...
+    
+    def __ge__(self, other: Any, /) -> bool:
+        """Greater than or equal comparison."""
+        ...
