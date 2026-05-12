2400151f6b3a122f9d540b768980012f32b6fe98
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2026-05-12 05:45:34 +0200

    Change NodeBase from class to interface

--- WinCopies/Collections/Generation/Factory/Keyable.py
+++ WinCopies/Collections/Generation/Factory/Keyable.py
@@ -3,35 +3,35 @@ from __future__ import annotations
 from abc import abstractmethod
 from weakref import ref, ReferenceType
 
-from WinCopies import Abstract
+from WinCopies import IInterface, Abstract
 from WinCopies.Collections import IGetter
 from WinCopies.Collections.Generation.Factory import IObjectFactory
 from WinCopies.Typing.Comparison import IHashableItem, HashableComparableProtocol
 
-def ExtractKey(item: NodeBase|object) -> object:
-    return item.GetKey() if isinstance(item, NodeBase) else item
+def ExtractKey(item: INodeBase|object) -> object:
+    return item.GetKey() if isinstance(item, INodeBase) else item
 def GetKey[TKey: HashableComparableProtocol, TValue](node: Node[TKey, TValue]) -> TKey:
     return node.GetKey()
 
-class NodeBase(Abstract):
+class INodeBase(IInterface):
     def __init__(self) -> None:
         super().__init__()
     
     @abstractmethod
     def GetKey(self) -> object:
         pass
-class Node[TKey: HashableComparableProtocol, TValue](NodeBase, IHashableItem["Node[TKey, TValue]|TKey"]):
+class Node[TKey: HashableComparableProtocol, TValue](Abstract, INodeBase, IHashableItem["Node[TKey, TValue]|TKey"]):
     def __init__(self, key: TKey, obj: TValue) -> None:
         super().__init__()
 
         self.__key: TKey = key
         self.__ref: ReferenceType[TValue] = ref(obj)
     
-    def TryGetValue(self) -> TValue|None:
-        return self.__ref()
-    
     def GetKey(self) -> TKey:
         return self.__key
+    
+    def TryGetValue(self) -> TValue|None:
+        return self.__ref()
     
     def Equals(self, item: Node[TKey, TValue]|TKey|object) -> bool:
         return self.GetKey() == ExtractKey(item)
