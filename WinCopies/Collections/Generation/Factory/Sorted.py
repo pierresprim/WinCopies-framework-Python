b35e96433894c4d8852e96f36d73826b1fb12123
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2026-05-12 05:49:22 +0200

    Add new type

--- WinCopies/Collections/Generation/Factory/Sorted.py
+++ WinCopies/Collections/Generation/Factory/Sorted.py
@@ -8,39 +8,45 @@ from WinCopies.Collections.Abstraction.Collection import SortedList
 from WinCopies.Collections.Extensions import ISortedList
 from WinCopies.Collections.Generation import IRemovable
 from WinCopies.Collections.Generation.Factory import ObjectFactoryBase, CompositeRemovable
-from WinCopies.Collections.Generation.Factory.Keyable import IKeyableObjectFactoryBase, IKeyableObjectFactory, Node, GetKey, ExtractKey
+from WinCopies.Collections.Generation.Factory.Keyable import IKeyableObjectFactoryBase, IKeyableObjectFactory, INode, Node, GetKey, ExtractKey
 from WinCopies.Collections.Linked.Doubly import IDoublyLinkedNode
 from WinCopies.Typing import INullable, GetNullableValue
 from WinCopies.Typing.Comparison import IExtendedComparable, HashableComparableProtocol, CompareTo
 from WinCopies.Typing.Object import WeakReference
 
+class ISortedNode[TKey, TValue](INode[TKey, TValue], IExtendedComparable['ISortedNode[TKey, TValue]|TKey']):
+    def __init__(self) -> None:
+        super().__init__()
+
 @final
-class _SortedNode[TKey: HashableComparableProtocol, TValue: IDisposableBase](Node[TKey, TValue], IExtendedComparable['_SortedNode[TKey, TValue]|TKey'], IRemovable):
-    def __init__(self, key: TKey, obj: TValue, items: ISortedList[_SortedNode[TKey, TValue]]) -> None:
+class _SortedNode[TKey: HashableComparableProtocol, TValue: IDisposableBase](Node[TKey, TValue], ISortedNode[TKey, TValue], IRemovable):
+    def __init__(self, key: TKey, obj: TValue, items: ISortedList[ISortedNode[TKey, TValue]]) -> None:
         super().__init__(key, obj)
 
-        self.__items: ISortedList[_SortedNode[TKey, TValue]] = items
+        self.__items: ISortedList[ISortedNode[TKey, TValue]] = items
     
     def CompareTo(self, item: _SortedNode[TKey, TValue]|TKey|object) -> bool|None:
         return CompareTo(self.GetKey(), ExtractKey(item))
     
     def Remove(self) -> None:
-        self.__items.RemoveAt(self.__items.BisectLeft(self.GetKey(), GetKey))
+        items: ISortedList[ISortedNode[TKey, TValue]] = self.__items
+
+        items.RemoveAt(items.BisectLeft(self.GetKey(), GetKey))
 
 class SortedObjectFactoryBase[TKey: HashableComparableProtocol, TIn, TOut: IDisposableBase](ObjectFactoryBase[TIn, TOut], IKeyableObjectFactoryBase[TKey, TIn, TOut]):
     def __init__(self) -> None:
         super().__init__()
 
-        self.__items: ISortedList[_SortedNode[TKey, TOut]] = SortedList[_SortedNode[TKey, TOut]]()
+        self.__items: ISortedList[ISortedNode[TKey, TOut]] = SortedList[ISortedNode[TKey, TOut]]()
     
     @final
-    def __TryGetNode(self, key: TKey) -> _SortedNode[TKey, TOut]|None:
-        items: ISortedList[_SortedNode[TKey, TOut]] = self._GetSortedItems()
+    def __TryGetNode(self, key: TKey) -> ISortedNode[TKey, TOut]|None:
+        items: ISortedList[ISortedNode[TKey, TOut]] = self._GetSortedItems()
 
         return items.TryGetValue(items.BisectLeft(key, GetKey)).TryGetValue()
     
     @final
-    def _GetSortedItems(self) -> ISortedList[_SortedNode[TKey, TOut]]:
+    def _GetSortedItems(self) -> ISortedList[ISortedNode[TKey, TOut]]:
         return self.__items
     
     @abstractmethod
@@ -48,7 +54,7 @@ class SortedObjectFactoryBase[TKey: HashableComparableProtocol, TIn, TOut: IDisp
         pass
     
     def _GetRemovable(self, obj: TOut, node: IDoublyLinkedNode[WeakReference[TOut]]) -> IRemovable:
-        items: ISortedList[_SortedNode[TKey, TOut]] = self._GetSortedItems()
+        items: ISortedList[ISortedNode[TKey, TOut]] = self._GetSortedItems()
         sortedNode: _SortedNode[TKey, TOut] = _SortedNode[TKey, TOut](self._GetKey(obj), obj, items)
 
         items.Add(sortedNode)
@@ -61,13 +67,13 @@ class SortedObjectFactoryBase[TKey: HashableComparableProtocol, TIn, TOut: IDisp
     
     @final
     def ContainsKey(self, key: TKey) -> bool:
-        node: _SortedNode[TKey, TOut]|None = self.__TryGetNode(key)
+        node: ISortedNode[TKey, TOut]|None = self.__TryGetNode(key)
 
         return node is not None and node.GetKey() == key
     
     @final
     def TryGetValue(self, key: TKey) -> INullable[TOut]:
-        node: _SortedNode[TKey, TOut]|None = self.__TryGetNode(key)
+        node: ISortedNode[TKey, TOut]|None = self.__TryGetNode(key)
         
         return GetNullableValue(None if node is None else (node.TryGetValue() if node.GetKey() == key else None))
     
