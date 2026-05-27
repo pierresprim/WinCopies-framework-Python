b206ad6d285c258553b0e8ff5f7e343edf1f88a3
pierresprim <49271108+pierresprim@users.noreply.github.com>
2026-05-27 02:19:05 +0200

    Add new functions

--- WinCopies/Collections/Iteration/__init__.py
+++ WinCopies/Collections/Iteration/__init__.py
@@ -9,7 +9,7 @@ from WinCopies.Collections.Enumeration.Selection import ExcluerEnumerator, Exclu
 from WinCopies.Collections.Util import MakeGenerator
 from WinCopies.Delegates import GetNotPredicate
 from WinCopies.Typing import INullable, GetNullable, GetNullValue
-from WinCopies.Typing.Delegate import Function, Predicate, Converter, NullableConverter, Selector
+from WinCopies.Typing.Delegate import Action, Function, Predicate, Converter, NullableConverter, Selector
 from WinCopies.Typing.Pairing import IKeyValuePair, CreateDualResult
 
 def TryEnumerate[T](items: Iterable[T]|None) -> Iterable[T]:
@@ -25,6 +25,45 @@ def TryEnumerate[T](items: Iterable[T]|None) -> Iterable[T]:
     return MakeGenerator() if items is None else items
 def TryGenerate[T](items: Generator[T]|None) -> Generator[T]:
     return MakeGenerator() if items is None else items
+
+def __DoForEach[T](items: Iterable[T], action: Action) -> Generator[T]:
+    for item in items:
+        action()
+
+        yield item
+def __ForEach[T](items: Iterable[T], action: Action) -> Generator[T]:
+    for item in items:
+        yield item
+
+        action()
+def __DoActionForEach[T](items: Iterable[T], pre: Action, post: Action) -> Generator[T]:
+    for item in items:
+        pre()
+
+        yield item
+
+        post()
+
+def DoForEach[T](items: Iterable[T]|None, action: Action) -> Generator[T]:
+    return MakeGenerator() if items is None else __DoForEach(items, action)
+def TryDoForEach[T](items: Iterable[T]|None, action: Action) -> Generator[T]|None:
+    return None if items is None else __DoForEach(items, action)
+def DoForEachValue[T](action: Action, *values: T) -> Generator[T]:
+    return __DoForEach(values, action)
+
+def ForEach[T](items: Iterable[T]|None, action: Action) -> Generator[T]:
+    return MakeGenerator() if items is None else __ForEach(items, action)
+def TryForEach[T](items: Iterable[T]|None, action: Action) -> Generator[T]|None:
+    return None if items is None else __ForEach(items, action)
+def ForEachValue[T](action: Action, *values: T) -> Generator[T]:
+    return __ForEach(values, action)
+
+def DoActionForEach[T](items: Iterable[T]|None, pre: Action, post: Action) -> Generator[T]:
+    return MakeGenerator() if items is None else __DoActionForEach(items, pre, post)
+def TryDoActionForEach[T](items: Iterable[T]|None, pre: Action, post: Action) -> Generator[T]|None:
+    return None if items is None else __DoActionForEach(items, pre, post)
+def DoActionForEachValue[T](pre: Action, post: Action, *values: T) -> Generator[T]:
+    return __DoActionForEach(values, pre, post)
 
 def Concatenate[T](collection: Iterable[Iterable[T]|None]|None) -> Generator[T]:
     """Concatenates multiple iterables into a single generator.
