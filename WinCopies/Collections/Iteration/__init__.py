d96fb5f261b764c6dd90f0e4e6a0ba57b01686a4
pierresprim <49271108+pierresprim@users.noreply.github.com>
2026-06-03 15:24:54 +0200

    Add new function

--- WinCopies/Collections/Iteration/__init__.py
+++ WinCopies/Collections/Iteration/__init__.py
@@ -82,6 +82,8 @@ def ConcatenateValues[T](*collection: Iterable[T]|None) -> Generator[T]:
 
 def ConcatenateItems[T](collection: Iterable[IEnumerable[T]|None]|None) -> Generator[T]:
     return Concatenate(Select(collection, lambda items: None if items is None else items.AsIterable()))
+def ConcatenateEnumerables[T](*collection: IEnumerable[T]|None) -> Generator[T]:
+    return ConcatenateItems(collection)
 
 def Append[T](items: Iterable[T]|None, values: Iterable[T]|None) -> Generator[T]:
     """Appends values to the end of items.
