fc0c91e7d3958f88427f82b6d97bf63d21edfccd
pierresprim <49271108+pierresprim@users.noreply.github.com>
2026-06-03 19:36:58 +0200

    Add new functions

--- WinCopies/Collections/Iteration/Loop.py
+++ WinCopies/Collections/Iteration/Loop.py
@@ -2,7 +2,7 @@ from collections.abc import Iterable
 
 from WinCopies.Collections import Generator
 from WinCopies.Collections.Util import MakeGenerator
-from WinCopies.Typing.Delegate import Action
+from WinCopies.Typing.Delegate import Action, Method
 
 def __DoForEach[T](items: Iterable[T], action: Action) -> Generator[T]:
     for item in items:
@@ -42,3 +42,44 @@ def TryDoActionForEach[T](items: Iterable[T]|None, pre: Action, post: Action) ->
     return None if items is None else __DoActionForEach(items, pre, post)
 def DoActionForEachValue[T](pre: Action, post: Action, *values: T) -> Generator[T]:
     return __DoActionForEach(values, pre, post)
+
+
+
+def __DoForEachItem[T](items: Iterable[T], action: Method[T]) -> Generator[T]:
+    for item in items:
+        action(item)
+
+        yield item
+def __ForEachItem[T](items: Iterable[T], action: Method[T]) -> Generator[T]:
+    for item in items:
+        yield item
+
+        action(item)
+def __DoActionForEachItem[T](items: Iterable[T], pre: Method[T], post: Method[T]) -> Generator[T]:
+    for item in items:
+        pre(item)
+
+        yield item
+
+        post(item)
+
+def DoForEachItem[T](items: Iterable[T]|None, action: Method[T]) -> Generator[T]:
+    return MakeGenerator() if items is None else __DoForEachItem(items, action)
+def TryDoForEachItem[T](items: Iterable[T]|None, action: Method[T]) -> Generator[T]|None:
+    return None if items is None else __DoForEachItem(items, action)
+def DoForEachItemValue[T](action: Method[T], *values: T) -> Generator[T]:
+    return __DoForEachItem(values, action)
+
+def ForEachItem[T](items: Iterable[T]|None, action: Method[T]) -> Generator[T]:
+    return MakeGenerator() if items is None else __ForEachItem(items, action)
+def TryForEachItem[T](items: Iterable[T]|None, action: Method[T]) -> Generator[T]|None:
+    return None if items is None else __ForEachItem(items, action)
+def ForEachValueItem[T](action: Method[T], *values: T) -> Generator[T]:
+    return __ForEachItem(values, action)
+
+def DoActionForEachItem[T](items: Iterable[T]|None, pre: Method[T], post: Method[T]) -> Generator[T]:
+    return MakeGenerator() if items is None else __DoActionForEachItem(items, pre, post)
+def TryDoActionForEachItem[T](items: Iterable[T]|None, pre: Method[T], post: Method[T]) -> Generator[T]|None:
+    return None if items is None else __DoActionForEachItem(items, pre, post)
+def DoActionForEachValueItem[T](pre: Method[T], post: Method[T], *values: T) -> Generator[T]:
+    return __DoActionForEachItem(values, pre, post)
