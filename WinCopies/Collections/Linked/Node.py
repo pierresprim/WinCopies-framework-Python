e768efaf35f2aa2ab68becd94ba736b03629a73c
pierresprim <49271108+pierresprim@users.noreply.github.com>
2026-06-09 00:36:57 +0200

    Rename LinkedNodeAbstract and LinkedNodeBase

--- WinCopies/Collections/Linked/Node.py
+++ WinCopies/Collections/Linked/Node.py
@@ -6,49 +6,45 @@ from typing import final
 from WinCopies import IInterface, Abstract
 
 class INode(IInterface):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self) -> None: super().__init__()
 
     @abstractmethod
     def GetNext(self) -> INode|None:
-        pass
+        ...
 class ITwoWayNode(INode):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self) -> None: super().__init__()
 
     @abstractmethod
     def GetPrevious(self) -> ITwoWayNode|None:
-        pass
+        ...
     @abstractmethod
     def GetNext(self) -> ITwoWayNode|None:
-        pass
+        ...
 
 class ILinkedNode[T](INode):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self) -> None: super().__init__()
     
     @abstractmethod
     def GetValue(self) -> T:
-        pass
+        ...
     @abstractmethod
     def SetValue(self, value: T) -> None:
-        pass
+        ...
 
     @abstractmethod
     def GetNext(self) -> ILinkedNode[T]|None:
-        pass
+        ...
 class ITwoWayLinkedNode[T](ILinkedNode[T], ITwoWayNode):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self) -> None: super().__init__()
 
     @abstractmethod
     def GetPrevious(self) -> ITwoWayLinkedNode[T]|None:
-        pass
+        ...
     @abstractmethod
     def GetNext(self) -> ITwoWayLinkedNode[T]|None:
-        pass
+        ...
 
-class LinkedNodeBase[T](Abstract):
+class LinkedNodeAbstract[T](Abstract):
     def __init__(self, value: T) -> None:
         super().__init__()
         
@@ -60,32 +56,29 @@ class LinkedNodeBase[T](Abstract):
     @final
     def SetValue(self, value: T) -> None:
         self.__value = value
-
-class LinkedNodeAbstract[TItem, TNode](LinkedNodeBase[TItem], ILinkedNode[TItem]):
+class LinkedNodeBase[TItem, TNode](LinkedNodeAbstract[TItem], ILinkedNode[TItem]):
     def __init__(self, value: TItem) -> None:
         super().__init__(value)
 
     @abstractmethod
-    def _AsLinkedNode(self, node: TNode) -> LinkedNodeAbstract[TItem, TNode]:
-        pass
-    def _TryAsLinkedNode(self, node: TNode|None) -> LinkedNodeAbstract[TItem, TNode]|None:
+    def _AsLinkedNode(self, node: TNode) -> LinkedNodeBase[TItem, TNode]:
+        ...
+    def _TryAsLinkedNode(self, node: TNode|None) -> LinkedNodeBase[TItem, TNode]|None:
         return None if node is None else self._AsLinkedNode(node)
     
     @abstractmethod
     def GetNextNode(self) -> TNode|None:
-        pass
-    def GetNext(self) -> ILinkedNode[TItem]|None:
-        return self._TryAsLinkedNode(self.GetNextNode())
+        ...
+    def GetNext(self) -> ILinkedNode[TItem]|None: return self._TryAsLinkedNode(self.GetNextNode())
 
-class LinkedNode[TItem, TNode](LinkedNodeAbstract[TItem, TNode]):
+class LinkedNode[TItem, TNode](LinkedNodeBase[TItem, TNode]):
     def __init__(self, value: TItem, nextNode: TNode|None) -> None:
         super().__init__(value)
 
         self.__next: TNode|None = nextNode
 
     @final
-    def GetNextNode(self) -> TNode|None:
-        return self.__next
+    def GetNextNode(self) -> TNode|None: return self.__next
     
     @final
     def _SetNext(self, nextNode: TNode|None) -> None:
@@ -93,6 +86,6 @@ class LinkedNode[TItem, TNode](LinkedNodeAbstract[TItem, TNode]):
 
     @abstractmethod
     def _AsLinkedNode(self, node: TNode) -> LinkedNode[TItem, TNode]:
-        pass
+        ...
     def _TryAsLinkedNode(self, node: TNode|None) -> LinkedNode[TItem, TNode]|None:
         return None if node is None else self._AsLinkedNode(node)
