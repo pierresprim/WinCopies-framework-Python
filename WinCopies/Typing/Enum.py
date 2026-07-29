4a1714f2d7350b9f15ba82371897a7117a8465bc
pierresprim <49271108+pierresprim@users.noreply.github.com>
2026-07-29 04:36:59 +0200

    Bug fixes

--- WinCopies/Typing/Enum.py
+++ WinCopies/Typing/Enum.py
@@ -2,7 +2,7 @@ from enum import Enum as _Enum, IntEnum as _IntEnum, StrEnum as _StrEnum
 from types import DynamicClassAttribute
 from typing import final, Self, Type, TypeVar
 
-from WinCopies.Typing.Comparison import IEquatableObjectBase, IHashableComparable
+from WinCopies.Typing.Comparison import IEquatableObjectBase, IHashable, IHashableComparable
 from WinCopies.Typing.Protocols import SupportsEqualityAndRichComparison
 
 T = TypeVar('T')
@@ -26,21 +26,29 @@ class Enum(IEquatableObjectBase[T]):
     @DynamicClassAttribute
     def value(self) -> T:
         return self._value_
-class OrderedEnum(Enum[U], IHashableComparable[U]):
+class ComparableEnum(Enum[U], IHashable[U]):
     def __init__(self, value: U) -> None: super().__init__(value)
     
     def __new__(cls, value: U) -> Self: return super().__new__(cls, value)
 
     @final
     def _AsComparableValue(self) -> U: return self.value
+class OrderedEnum(ComparableEnum[U], IHashableComparable[U]):
+    def __init__(self, value: U) -> None: super().__init__(value)
+    
+    def __new__(cls, value: U) -> Self: return super().__new__(cls, value)
 
 class IntEnum(OrderedEnum[int], _Enum):
+    def __init__(self, value: int) -> None: super().__init__(value)
+
     def __new__(cls, value: int) -> Self: return super().__new__(cls, value)
 
     @classmethod
     @final
     def _GetComparableType(cls) -> Type[int]: return int
-class StrEnum(Enum[str], _Enum):
+class StrEnum(ComparableEnum[str], _Enum):
+    def __init__(self, value: str) -> None: super().__init__(value)
+    
     def __new__(cls, value: str) -> Self: return super().__new__(cls, value)
 
     @classmethod
