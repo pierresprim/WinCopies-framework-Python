4234406d79c7967a14697400d3ba4d512d6437c8
pierresprim <49271108+pierresprim@users.noreply.github.com>
2026-08-02 19:42:33 +0200

    Update protocols

--- WinCopies/Typing/Protocols.py
+++ WinCopies/Typing/Protocols.py
@@ -1,6 +1,11 @@
 from typing import runtime_checkable, Any, Protocol, Self
 
 @runtime_checkable
+class SupportsStringization(Protocol):
+    def __str__(self) -> str:
+        ...
+
+@runtime_checkable
 class SupportsEqualityComparison(Protocol):
     """Protocol for types that support equality comparison operator."""
     
@@ -26,21 +31,12 @@ class SupportsRichComparison(Protocol):
         ...
 
 @runtime_checkable
-class SupportsEqualityAndRichComparison(Protocol):
-    def __eq__(self, other: Any, /) -> bool:
-        ...
-    def __hash__(self, /) -> int:
-        ...
-    
-    def __lt__(self, other: Self, /) -> bool:
-        """Less than comparison."""
-        ...
-    def __le__(self, other: Self, /) -> bool:
-        """Less than or equal comparison."""
-        ...
-    def __gt__(self, other: Self, /) -> bool:
-        """Greater than comparison."""
-        ...
-    def __ge__(self, other: Self, /) -> bool:
-        """Greater than or equal comparison."""
-        ...
+class SupportsEqualityAndRichComparison(SupportsEqualityComparison, SupportsRichComparison, Protocol):
+    pass
+
+@runtime_checkable
+class EquatableObject(SupportsEqualityComparison, SupportsStringization, Protocol):
+    pass
+@runtime_checkable
+class ComparableObject(SupportsEqualityAndRichComparison, EquatableObject, Protocol):
+    pass
