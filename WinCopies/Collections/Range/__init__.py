38659ba1563c90720bef2472a6fc76e3f168a093
pierresprim <49271108+pierresprim@users.noreply.github.com>
2026-08-06 20:02:13 +0200

    Add new functions

--- WinCopies/Collections/Range/__init__.py
+++ WinCopies/Collections/Range/__init__.py
@@ -1,15 +1,39 @@
-from collections.abc import Iterable, Sequence
-from typing import SupportsIndex
+from collections.abc import Iterable, Sequence, MutableSequence
+from typing import overload, SupportsIndex
 
 from WinCopies.Collections.Abstraction.Collection import CreateTuple
-from WinCopies.Collections.Core import IList
+from WinCopies.Collections.Core import ITuple as ITupleBase
 from WinCopies.Collections.Enumeration import ICountableEnumerable
-from WinCopies.Collections.Extensions import ITuple
+from WinCopies.Collections.Extensions import ITuple, IList
 from WinCopies.Collections.Linked.Singly import CreateCountableQueue, CreateEnumerableStack
 from WinCopies.Collections.Util import ReverseIndex
 
-def GetItems[T](l: ITuple[T], index: SupportsIndex|slice) -> T|Sequence[T]:
-    return l.GetAt(int(index)) if isinstance(index, SupportsIndex) else l.SliceAt(index).AsSequence()
+def GetAt[T](l: ITupleBase[T], index: SupportsIndex) -> T:
+    return l.GetAt(int(index))
+
+@overload
+def GetValues[T](l: IList[T], index: slice) -> MutableSequence[T]: ...
+@overload
+def GetValues[T](l: ITuple[T], index: slice) -> Sequence[T]: ...
+
+def GetValues[T](l: ITuple[T]|IList[T], index: slice) -> Sequence[T]|MutableSequence[T]:
+    return l.SliceAt(index).AsSequence()
+
+@overload
+def GetItems[T](l: IList[T], index: SupportsIndex|slice) -> T|MutableSequence[T]: ...
+@overload
+def GetItems[T](l: ITuple[T], index: SupportsIndex|slice) -> T|Sequence[T]: ...
+
+def GetItems[T](l: ITuple[T]|IList[T], index: SupportsIndex|slice) -> T|Sequence[T]|MutableSequence[T]:
+    return GetAt(l, index) if isinstance(index, SupportsIndex) else GetValues(l, index)
+
+@overload
+def GetItemsAt[T](l: IList[T], index: SupportsIndex|slice) -> T|IList[T]: ...
+@overload
+def GetItemsAt[T](l: ITuple[T], index: SupportsIndex|slice) -> T|ITuple[T]: ...
+
+def GetItemsAt[T](l: ITuple[T]|IList[T], index: SupportsIndex|slice) -> T|ITuple[T]|IList[T]:
+    return GetAt(l, index) if isinstance(index, SupportsIndex) else l.SliceAt(index)
 
 def SetValues[T](lst: IList[T], key: slice, values: Iterable[T]) -> None:
     def reverseIndex(index: int) -> int: return ReverseIndex(index, lst.GetCount())
