80cb0829e8b3942b79f129b7206df702b1ebfe54
pierresprim <49271108+pierresprim@users.noreply.github.com>
2026-08-06 19:58:03 +0200

    Bug fix

--- WinCopies/Collections/Abstraction/Mapping/Extensions.py
+++ WinCopies/Collections/Abstraction/Mapping/Extensions.py
@@ -83,10 +83,7 @@ class _OrderedSetList[T: HashableProtocol](Abstract, MutableList[T], Collection.
         return False
     
     @final
-    def _TryRemoveRange(self, index: int, count: int) -> bool:
-        for i in range(count): self.RemoveAt(index + i)
-
-        return True
+    def _TryRemoveRange(self, index: int, count: int) -> bool: return super(MutableList[T], self)._TryRemoveRange(index, count)
     
     def TryRemoveAt(self, index: int) -> bool|None:
         if index < 0: return None
