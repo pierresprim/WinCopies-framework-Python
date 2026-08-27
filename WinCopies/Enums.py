4b524bad6383f92c3e424822734906edf2a9b28f
pierresprim <49271108+pierresprim@users.noreply.github.com>
2026-08-27 18:01:33 +0200

    Update the Enums module

--- WinCopies/Enums.py
+++ WinCopies/Enums.py
@@ -1,19 +1,23 @@
-from enum import Enum
-
 from WinCopies.Typing.Enum import IntEnum
 
-class Endianness(Enum):
+class Endianness(IntEnum):
     Null = 0
     Little = 1
     Big = 2
 
-class Sign(Enum):
+class Sign(IntEnum):
     Signed = 1
     Unsigned = 2
     Float = 3
 
 class BitDepthLevel(IntEnum):
     One = 8
-    Two = 16
-    Three = 32
-    Four = 64
+    Two = One << 1
+    Three = One << 2
+    Four = One << 3
+    Five = One << 4
+    Six = One << 5
+    Seven = One << 6
+    Eight = One << 7
+    Nine = One << 8
+    Ten = One << 9
