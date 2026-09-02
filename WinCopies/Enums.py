2da61a729962e82bf4b3b8ed9a69cdb5f4824a78
pierresprim <49271108+pierresprim@users.noreply.github.com>
2026-09-02 08:37:51 +0200

    Update Enums.py

--- WinCopies/Enums.py
+++ WinCopies/Enums.py
@@ -1,3 +1,6 @@
+from collections.abc import Sequence
+from enum import auto
+
 from WinCopies.Typing.Enum import IntEnum, StrEnum
 
 class Endianness(IntEnum):
@@ -11,16 +14,20 @@ class Sign(IntEnum):
     Float = 3
 
 class BitDepthLevel(IntEnum):
-    One = 8
-    Two = One << 1
-    Three = One << 2
-    Four = One << 3
-    Five = One << 4
-    Six = One << 5
-    Seven = One << 6
-    Eight = One << 7
-    Nine = One << 8
-    Ten = One << 9
+    @staticmethod
+    def _generate_next_value_(name: str, start: int, count: int, last_values: Sequence[int]) -> int:
+        return 8 << count
+    
+    One = auto()
+    Two = auto()
+    Three = auto()
+    Four = auto()
+    Five = auto()
+    Six = auto()
+    Seven = auto()
+    Eight = auto()
+    Nine = auto()
+    Ten = auto()
 
 class ErrorMessages(StrEnum):
     ReentrancyNotAllowed = "Reentrant calls are not supported: this method cannot be called while another operation is in progress on this object."
