db26e2aa4167bfc5f08321bbb15b18da707cf7fa
pierresprim <49271108+pierresprim@users.noreply.github.com>
2026-09-04 03:01:39 +0200

    Add new types

--- WinCopies/Collections/Generation/Registry/__init__.py
+++ WinCopies/Collections/Generation/Registry/__init__.py
@@ -1,6 +1,8 @@
 from abc import abstractmethod
 
-from WinCopies import IInterface
+from WinCopies import IInterface, Abstract
+from WinCopies.Collections.Generation import IRemovable
+from WinCopies.Typing.Discard import IInvalidatable
 
 class IObjectMonitor(IInterface):
     def __init__(self) -> None: super().__init__()
@@ -13,4 +15,31 @@ class IObjectRegistry[T](IObjectMonitor):
     
     @abstractmethod
     def RegisterObject(self, item: T) -> None:
+        ...
+
+class IInvalidationRegistrarBase(IInterface):
+    def __init__(self) -> None: super().__init__()
+
+    @abstractmethod
+    def Unregister(self) -> None:
+        ...
+
+class IInvalidationRegistrar(IInvalidationRegistrarBase):
+    def __init__(self) -> None: super().__init__()
+
+    @abstractmethod
+    def Register(self, cookie: IInvalidatable) -> None:
+        ...
+class InvalidationRegistrar(Abstract, IInvalidationRegistrar):
+    def __init__(self) -> None: super().__init__()
+
+class IManagedInvalidationRegistrar(IInvalidationRegistrarBase):
+    def __init__(self) -> None: super().__init__()
+
+    @abstractmethod
+    def Push(self, invalidationRegistrar: IInvalidationRegistrar) -> IRemovable:
+        ...
+
+    @abstractmethod
+    def Register(self) -> None:
         ...
