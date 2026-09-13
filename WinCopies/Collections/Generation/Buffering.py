6df4c899d988fc7776d2b6d834b3e257d323e0fe
pierresprim <49271108+pierresprim@users.noreply.github.com>
2026-09-13 02:46:19 +0200

    Add new modules

---
+++ WinCopies/Collections/Generation/Buffering.py
@@ -0,0 +1,157 @@
+from __future__ import annotations
+
+from abc import abstractmethod
+from collections.abc import Iterable
+from typing import final
+
+from WinCopies import IInterface, Abstract
+from WinCopies.Collections import Generator
+from WinCopies.Collections.Iteration import PrependValues, Select
+from WinCopies.Collections.Util import MakeSequence
+from WinCopies.Typing import INullable, GetNullable, GetNullValue
+from WinCopies.Typing.Delegate import Method
+
+class _BuilderNode[T](Abstract):
+    def __init__(self, value: T) -> None:
+        super().__init__()
+
+        self.__value: T = value
+        self.__next: _BuilderNode[T]|None = None
+
+    @final
+    def GetValue(self) -> T:
+        return self.__value
+
+    @final
+    def SetNext(self, value: T) -> _BuilderNode[T]:
+        next: _BuilderNode[T] = _BuilderNode[T](value)
+
+        self.__next = next
+
+        return next
+
+    @final
+    def RemoveNext(self) -> _BuilderNode[T]|None:
+        next: _BuilderNode[T]|None = self.__next
+
+        self.__next = None
+
+        return next
+
+type BuildResult[T] = tuple[T, Iterable[T]|None]
+
+class IBuilderBase[T](IInterface):
+    def __init__(self) -> None: super().__init__()
+
+    @abstractmethod
+    def Push(self, value: T) -> None:
+        ...
+class IBuilder[T](IBuilderBase[T]):
+    def __init__(self) -> None: super().__init__()
+    
+    @abstractmethod
+    def Build(self) -> Iterable[T]|None:
+        ...
+    @abstractmethod
+    def TryBuild(self) -> INullable[T]|Iterable[T]:
+        ...
+class Builder[T](Abstract, IBuilder[T]):
+    def __init__(self) -> None:
+        self.__first: _BuilderNode[T]|None = None
+        self.__last: _BuilderNode[T]|None = None
+
+        def push(value: T) -> None:
+            def push(value: T) -> None:
+                last: _BuilderNode[T] = self.__last # type: ignore[assignment]
+
+                self.__last = last.SetNext(value) # pyright: ignore[reportOptionalMemberAccess]
+
+            first: _BuilderNode[T] = _BuilderNode[T](value)
+
+            self.__first = first
+            self.__last = first
+
+            self.__push = push
+
+        super().__init__()
+
+        self.__push: Method[T] = push # type: ignore[no-redef]
+
+    @final
+    def __Build(self, includeFirst: bool) -> BuildResult[T]|None:
+        def build(first: _BuilderNode[T]) -> BuildResult[T]:
+            def build(node: _BuilderNode[T]|None) -> Generator[_BuilderNode[T]]:
+                while node is not None:
+                    yield node
+
+                    node = node.RemoveNext()
+            
+            def makeSequence(nodes: Iterable[_BuilderNode[T]]) -> Iterable[T]:
+                return Select(nodes, lambda node: node.GetValue())
+            
+            def createTuple(items: Iterable[T]|None) -> BuildResult[T]:
+                return first.GetValue(), items
+
+            next: _BuilderNode[T]|None = first.RemoveNext()
+
+            return createTuple(makeSequence(MakeSequence(first)) if includeFirst else None) if next is None else createTuple(makeSequence(PrependValues(build(next.RemoveNext()), first, next)))
+        
+        first: _BuilderNode[T]|None = self.__first
+
+        if first is None: return None
+
+        self.__first = None
+
+        return build(first)
+
+    @final
+    def Push(self, value: T) -> None:
+        self.__push(value)
+
+    @final
+    def Build(self) -> Iterable[T]|None:
+        result: BuildResult[T]|None = self.__Build(True)
+
+        return None if result is None else result[1]
+    @final
+    def TryBuild(self) -> INullable[T]|Iterable[T]:
+        result: BuildResult[T]|None = self.__Build(False)
+
+        if result is None: return GetNullValue()
+
+        items: Iterable[T]|None = result[1]
+
+        return GetNullable(result[0]) if items is None else items
+
+class IAbstractionBuilder[TItem, TList](IBuilderBase[TItem]):
+    def __init__(self) -> None: super().__init__()
+    
+    @abstractmethod
+    def Build(self) -> TList|None:
+        ...
+    @abstractmethod
+    def TryBuild(self) -> INullable[TItem]|TList:
+        ...
+class AbstractionBuilder[TItem, TList](Abstract, IAbstractionBuilder[TItem, TList]):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.__builder: IBuilder[TItem] = Builder[TItem]()
+
+    @abstractmethod
+    def _CreateList(self, items: Iterable[TItem]) -> TList:
+        ...
+
+    @final
+    def Push(self, value: TItem) -> None: return self.__builder.Push(value)
+
+    @final
+    def Build(self) -> TList|None:
+        items: Iterable[TItem]|None = self.__builder.Build()
+
+        return None if items is None else self._CreateList(items)
+    @final
+    def TryBuild(self) -> INullable[TItem]|TList:
+        result: INullable[TItem]|Iterable[TItem] = self.__builder.TryBuild()
+
+        return result if isinstance(result, INullable) else self._CreateList(result)
