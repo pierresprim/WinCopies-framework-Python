2557057f98cf3a151ce437555aa15d83c8c9f9b7
pierresprim <49271108+pierresprim@users.noreply.github.com>
2026-09-13 17:38:54 +0200

    Update I/ExceptionGroupBuilder

--- WinCopies/Typing/Extensions.py
+++ WinCopies/Typing/Extensions.py
@@ -1,13 +1,22 @@
 
 from typing import Iterable
 
-from WinCopies.Collections.Generation.Buffering import IAbstractionBuilder, AbstractionBuilder
+from WinCopies.Collections.Generation.Buffering import IGroupBuilder, GroupBuilder
 from WinCopies.Collections.Util import MakeSequence
 
-class IExceptionGroupBuilder(IAbstractionBuilder[Exception, ExceptionGroup]):
-    def __init__(self) -> None: super().__init__()
-class ExceptionGroupBuilder(AbstractionBuilder[Exception, ExceptionGroup], IExceptionGroupBuilder):
+class IExceptionGroupBuilder(IGroupBuilder[Exception, ExceptionGroup]):
     def __init__(self) -> None: super().__init__()
 
-    def _CreateList(self, items: Iterable[Exception]) -> ExceptionGroup[Exception]:
-        return ExceptionGroup("Multiple exceptions occurred.", MakeSequence(*items))
+    def TryThrow(self) -> None:
+        ...
+class ExceptionGroupBuilder(GroupBuilder[Exception, ExceptionGroup], IExceptionGroupBuilder):
+    def __init__(self) -> None: super().__init__()
+
+    def _CreateList(self, items: Iterable[Exception]) -> ExceptionGroup[Exception]: return ExceptionGroup("Multiple exceptions occurred.", MakeSequence(*items))
+
+    def _AsContainer(self, container: ExceptionGroup[Exception]) -> Exception: return container
+
+    def TryThrow(self) -> None:
+        exception: Exception|None = self.TryBuildGroup().TryGetValue()
+        
+        if exception is not None: raise exception
